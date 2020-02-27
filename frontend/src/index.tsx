1b3b094305937837d0b43c292a649be2b005a1ca
Martin Førre <martin.forre@bouvet.no>
2020-02-27 12:58:00 +0100

    Add fusion app

---
+++ frontend/src/index.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { registerApp, useCurrentUser, useNotificationCenter } from '@equinor/fusion';
+import { Button, usePopoverRef } from '@equinor/fusion-components';
+
+import * as styles from './styles.less';
+
+const App: React.FC = () => {
+    const currentUser = useCurrentUser();
+    const sendNotification = useNotificationCenter();
+
+    const [popoverRef] = usePopoverRef(
+        <div className={styles.popover}>What a lovely popover 💩</div>,
+        {
+            placement: 'below',
+        }
+    );
+    
+    const sendWelcomeNotification = async () => {
+        await sendNotification({
+            id: 'This is a unique id which means the notification will only be shown once',
+            level: 'medium',
+            title:
+                'Welcome to your new fusion app! Open up src/index.tsx to start building your app!',
+        });
+    };
+
+    React.useEffect(() => {
+        sendWelcomeNotification();
+    }, []);
+
+    if (!currentUser) {
+        return null;
+    }
+
+    return (
+        <div className={styles.hello}>
+            <h1>Oh hello there, {currentUser.fullName}</h1>
+            <Button ref={popoverRef}>Click me!</Button>
+        </div>
+    );
+};
+
+registerApp('resources', {
+    AppComponent: App,
+});
+
+if (module.hot) {
+    module.hot.accept();
+}
