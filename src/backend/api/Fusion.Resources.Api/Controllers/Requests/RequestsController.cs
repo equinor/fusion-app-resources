3c19a1632238e09bc24f7c369b00cb417dc55646
Hans Dahle <HANDAH@equinor.com>
2020-02-27 17:40:55 +0100

    added request controller

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/Requests/RequestsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+
+    }
+
+
+    public class ApiContractPersonnelRequest
+    {
+
+
+    }
+}
