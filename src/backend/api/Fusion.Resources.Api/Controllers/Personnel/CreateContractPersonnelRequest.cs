9917cf3d4b21d122635e57768dd55e08619e972a
Hans Dahle <HANDAH@equinor.com>
2020-02-27 16:46:07 +0100

    Api prototype

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/CreateContractPersonnelRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Fusion.Resources.Api.Controllers
+{
+    public class CreateContractPersonnelRequest
+    {
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+    }
+}
