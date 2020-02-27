2978bfbbf262e06bd7aabd49c00d2e6e32f95c92
Martin Førre <martin.forre@bouvet.no>
2020-02-27 13:01:33 +0100

    Clean page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,41 +1,11 @@
 import * as React from 'react';
-import { registerApp, useCurrentUser, useNotificationCenter } from '@equinor/fusion';
-import { Button, usePopoverRef } from '@equinor/fusion-components';
-
-import * as styles from './styles.less';
+import { registerApp } from '@equinor/fusion';
 
 const App: React.FC = () => {
-    const currentUser = useCurrentUser();
-    const sendNotification = useNotificationCenter();
-
-    const [popoverRef] = usePopoverRef(
-        <div className={styles.popover}>What a lovely popover 💩</div>,
-        {
-            placement: 'below',
-        }
-    );
     
-    const sendWelcomeNotification = async () => {
-        await sendNotification({
-            id: 'This is a unique id which means the notification will only be shown once',
-            level: 'medium',
-            title:
-                'Welcome to your new fusion app! Open up src/index.tsx to start building your app!',
-        });
-    };
-
-    React.useEffect(() => {
-        sendWelcomeNotification();
-    }, []);
-
-    if (!currentUser) {
-        return null;
-    }
-
     return (
-        <div className={styles.hello}>
-            <h1>Oh hello there, {currentUser.fullName}</h1>
-            <Button ref={popoverRef}>Click me!</Button>
+        <div>
+            Resources
         </div>
     );
 };
