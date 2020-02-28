52c24c0f03d19930b8c0f8d04026182e3d3d77f1
Ole-Richard Blindheim <oblin@equinor.com>
2020-02-28 09:59:51 +0100

    ManagePersonnel datatable

---
+++ src/frontend/src/pages/ManagePersonnel/PersonnelColumn.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { DataTableColumn } from '@equinor/fusion-components';
+import Personnel from '../../models/Personnel';
+
+export type DataItemProps = {
+  item: Personnel;
+  rowIndex: number;
+};
+
+const AzureAdStatus : React.FC<DataItemProps> = ({ item }) => (
+  <div>{item.AzureAdStatus}</div>
+)
+
+
+const PersonnelColumn: DataTableColumn<Personnel>[] = [
+  {
+    key: 'Name',
+    accessor: 'Name',
+    label: 'Person',
+    priority: 1,
+    sortable: true,
+  },
+  {
+    key: 'Mail',
+    accessor: 'Mail',
+    label: 'E-Mail',
+    priority: 5,
+    sortable: true,
+  },
+  {
+    key: 'AzureAdStatus',
+    accessor: 'AzureAdStatus',
+    label: 'AD',
+    priority: 10,
+    component : AzureAdStatus,
+    sortable: true,
+  },
+  {
+    key: 'Phone',
+    accessor: 'PhoneNumber',
+    label: 'Phone Number',
+    priority: 15,
+    sortable: true,
+  },
+  {
+    key: 'Disciplines',
+    accessor: 'Disciplines',
+    label: 'Disciplines',
+    priority: 20,
+    sortable: true,
+  },
+]
+
+
+export default PersonnelColumn
