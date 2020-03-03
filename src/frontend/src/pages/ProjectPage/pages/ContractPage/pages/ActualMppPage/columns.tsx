b6267ec9cd5cc91964972ce3a30d263612e3f70d
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-03-03 15:31:40 +0100

    Added assigned person component

--- src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ActualMppPage/columns.tsx
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ActualMppPage/columns.tsx
@@ -1,20 +1,29 @@
-import { DataTableColumn } from '@equinor/fusion-components';
+import { DataTableColumn, PersonCard } from '@equinor/fusion-components';
 import * as React from 'react';
 import PositionColumn from '../../../../components/PositionColumn';
 import { Position } from '@equinor/fusion';
 
+type AssignedPersonProps = {
+    item: Position,
+};
+const AssignedPersonComponent: React.FC<AssignedPersonProps> = ({ item }) => {
+    const person = item.instances.find(i => i.assignedPerson)?.assignedPerson || undefined
+    return <PersonCard person={person} photoSize="medium" inline />;
+};
+
 
 const columns: DataTableColumn<Position>[] = [
-    {
-        accessor: request => request.basePosition?.name || 'TBN',
-        key: 'basePosition',
-        label: 'Base position',
-        sortable: true,
-    },
     {
         accessor: request => request.instances.find(i => i.assignedPerson?.name)?.assignedPerson?.name || '',
         key: 'person',
         label: 'Person',
+        sortable: true,
+        component: AssignedPersonComponent
+    },
+    {
+        accessor: request => request.basePosition?.name || 'TBN',
+        key: 'basePosition',
+        label: 'Base position',
         sortable: true,
     },
     {
