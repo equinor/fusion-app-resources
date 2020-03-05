c707aef9f57dc63da8b3e237163d1ad3fff44215
Hans Dahle <HANDAH@equinor.com>
2020-03-05 10:14:28 +0100

    Removed bogus personnel

--- src/backend/api/Fusion.Resources.Api/Controllers/Personnel/PersonnelController.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/PersonnelController.cs
@@ -30,45 +30,9 @@ namespace Fusion.Resources.Api.Controllers
         [HttpGet("/projects/{projectIdentifier}/contracts/{contractIdentifier}/resources/personnel")]
         public async Task<ActionResult<ApiCollection<ApiContractPersonnel>>> GetContractPersonnel([FromRoute]ProjectIdentifier projectIdentifier, Guid contractIdentifier, [FromQuery]ODataQueryParams query) 
         {
-
             var contractPersonnel = await DispatchAsync(new GetContractPersonnel(contractIdentifier, query));
 
-
-            var personnel = new Faker<ApiContractPersonnel>()
-                .RuleFor(p => p.PersonnelId, f => Guid.NewGuid())
-                .RuleFor(p => p.AzureUniquePersonId, f => f.PickRandom<Guid?>(new[] { (Guid?)null, Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() }))
-                .RuleFor(p => p.Name, f => f.Person.FullName)
-                .RuleFor(p => p.Mail, f => f.Person.Email)
-                .RuleFor(p => p.JobTitle, f => f.Name.JobTitle())
-                .RuleFor(p => p.PhoneNumber, f => f.Person.Phone)
-                .RuleFor(p => p.HasCV, f => f.Random.Bool())
-                .RuleFor(p => p.Created, f => f.Date.PastOffset())
-                .RuleFor(p => p.Updated, f => f.PickRandom(new[] { (DateTimeOffset?)null, (DateTimeOffset?)null, f.Date.PastOffset() }))
-                .RuleFor(p => p.AzureAdStatus, f => f.PickRandomWithout<ApiContractPersonnel.ApiAccountStatus>(ApiContractPersonnel.ApiAccountStatus.NoAccount))
-                .FinishWith((f, p) =>
-                {
-                    if (p.AzureUniquePersonId == null)
-                    {
-                        p.AzureAdStatus = ApiContractPersonnel.ApiAccountStatus.NoAccount;
-                    }
-
-                    p.Disciplines = Enumerable.Range(0, f.Random.Number(1, 4)).Select(i => new ApiPersonnelDiscipline(f.Hacker.Adjective())).ToList();
-                })
-                .Generate(new Random().Next(50, 200));
-
-            if (query.HasFilter)
-            {
-                personnel = personnel.AsQueryable().ApplyODataFilters(query, m =>
-                {
-                    m.MapField("azureAdStatus", p => p.AzureAdStatus);
-                    m.MapField("name", p => p.Name);
-                    m.MapField("phoneNumber", p => p.PhoneNumber);
-                    m.MapField("created", p => p.Created);
-                })
-                .ToList();
-            }
-
-            var returnItems = contractPersonnel.Select(p => new ApiContractPersonnel(p)).Union(personnel);
+            var returnItems = contractPersonnel.Select(p => new ApiContractPersonnel(p));
 
             var collection = new ApiCollection<ApiContractPersonnel>(returnItems);
             return collection;
