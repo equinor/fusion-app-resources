b64a4afe4efb054cfa806079267ab6e8da0ef4d3
Hans Dahle <HANDAH@equinor.com>
2020-03-06 07:27:05 +0100

    Moved to powershell scripts

---
+++ src/backend/infrastructure/deploy-resources.ps1
@@ -0,0 +1,29 @@
+param(
+    [string]$environment,
+    [string]$clientId
+)
+
+Write-Host "Starting deployment of general resources"
+
+$resourceGroup = "fusion-apps-resources-$environment"
+
+Write-Host "Using resource group $resourceGroup"      
+
+
+$clientSecretName = "ClientSecret-Resources-Test"
+$adClientSecret = Get-AzKeyVaultSecret -VaultName ProView-Shared-Secrets -Name $clientSecretName
+
+Write-Host "Deploying template"
+
+New-AzResourceGroupDeployment -Mode Incremental -Name "fusion-app-resources-environment" -ResourceGroupName $resourceGroup -TemplateFile  "$(Build.SourcesDirectory)/src/backend/infrastructure/arm-templates/environment.template.json" `
+    -env-name $environment `
+    -aad-client-secret $adClientSecret.SecretValue `
+    -sql-connection-string $env:SQLCONNECTIONSTRING
+
+Write-Host "Setting service principal key vault access"
+$spName = (Get-AzContext).Account.Id
+Set-AzKeyVaultAccessPolicy -VaultName "keys-fusion-resources-$environment" -ServicePrincipalName $spName -PermissionsToSecrets get,list,set
+
+Write-Host "Setting ad app service principal key vault access"
+$appSpId = (Get-AzADServicePrincipal -ApplicationId $clientId).Id
+Set-AzKeyVaultAccessPolicy -VaultName "keys-fusion-resources-$environment" -ObjectId $appSpId -PermissionsToSecrets get,list
