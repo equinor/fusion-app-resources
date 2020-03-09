684742570e001109d2bbb6a87b33531a4a426bde
Ole-Richard Blindheim <oblin@equinor.com>
2020-03-09 08:11:29 +0100

    Added filter pane with search to manage personnel page

--- src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ManagePersonnelPage/index.tsx
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ManagePersonnelPage/index.tsx
@@ -1,20 +1,31 @@
-import * as React from 'react'
+import * as React from 'react';
 import { DataTable, DataTableColumn, Button, AddIcon } from '@equinor/fusion-components';
 import { useSorting, useCurrentContext } from '@equinor/fusion';
 import PersonnelColumns from './PersonnelColumns';
 import usePersonnel from './hooks/usePersonnel';
 import Personnel from '../../../../../../models/Personnel';
-import * as styles from './styles.less'
+import * as styles from './styles.less';
 import { useContractContext } from '../../../../../../contractContex';
-import AddPersonnelSideSheet from './AddPersonnelSideSheet'
-
-
+import AddPersonnelSideSheet from './AddPersonnelSideSheet';
+import getFilterSections from './getFilterSections';
+import GenericFilter from '../../../../../../components/GenericFilter';
 
 const ManagePersonnelPage: React.FC = () => {
-    const currentContext = useCurrentContext()
-    const currentContract = useContractContext()
-    const { personnel, isFetchingPersonnel, personnelError } = usePersonnel(currentContract?.contract?.id || undefined, currentContext?.id);
-    const { sortedData, setSortBy, sortBy, direction } = useSorting<Personnel>(personnel, "name", "asc");
+    const currentContext = useCurrentContext();
+    const currentContract = useContractContext();
+    const { personnel, isFetchingPersonnel, personnelError } = usePersonnel(
+        currentContract?.contract?.id || undefined,
+        currentContext?.id
+    );
+    const [filteredPersonnel, setFilteredPersonnel] = React.useState<Personnel[]>(
+        []
+    );
+
+    const { sortedData, setSortBy, sortBy, direction } = useSorting<Personnel>(
+        filteredPersonnel,
+        'name',
+        'asc'
+    );
     const [isAddPersonOpen, setIsAddPersonOpen] = React.useState<boolean>(false);
     const [selectedItems, setSelectedItems] = React.useState<Personnel[]>([]);
 
@@ -25,13 +36,26 @@ const ManagePersonnelPage: React.FC = () => {
         [sortBy, direction]
     );
 
+    const filterSections = React.useMemo(() => {
+        return getFilterSections(filteredPersonnel);
+    }, [filteredPersonnel]);
+
     const personnelColumns = React.useMemo(() => PersonnelColumns(), []);
-    const sortedByColumn = React.useMemo(() => personnelColumns.find(c => c.accessor === sortBy) || null, [sortBy]);
+    const sortedByColumn = React.useMemo(
+        () => personnelColumns.find(c => c.accessor === sortBy) || null,
+        [sortBy]
+    );
 
     return (
+
         <div className={styles.container}>
             <div className={styles.managePersonnel}>
-                <div className={styles.toolbar}><Button outlined onClick={() => setIsAddPersonOpen(true)} >  <AddIcon />  Add Person </Button></div>
+                <div className={styles.toolbar}>
+                    <Button outlined onClick={() => setIsAddPersonOpen(true)}>
+                        {' '}
+                        <AddIcon /> Add Person{' '}
+                    </Button>
+                </div>
                 <DataTable
                     columns={personnelColumns}
                     data={sortedData}
@@ -46,10 +70,21 @@ const ManagePersonnelPage: React.FC = () => {
                     onSelectionChange={setSelectedItems}
                     selectedItems={selectedItems}
                 />
-                {isAddPersonOpen && <AddPersonnelSideSheet isOpen={isAddPersonOpen} setIsOpen={setIsAddPersonOpen} selectedPersonnel={selectedItems.length ? selectedItems : null} />}
+                {isAddPersonOpen && (
+                    <AddPersonnelSideSheet
+                        isOpen={isAddPersonOpen}
+                        setIsOpen={setIsAddPersonOpen}
+                        selectedPersonnel={selectedItems.length ? selectedItems : null}
+                    />
+                )}
             </div>
+            <GenericFilter
+                data={personnel}
+                filterSections={filterSections}
+                onFilter={setFilteredPersonnel}
+            />
         </div>
     );
-}
+};
 
-export default ManagePersonnelPage
+export default ManagePersonnelPage;
