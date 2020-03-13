99164829e4df08c9a08205e4cbf41f0752eea2c7
Leif Bjarte Johansson <lejoh@equinor.com>
2020-03-13 09:27:47 +0100

    Sync infrastructure

---
+++ src/backend/function/Fusion.Resources.Functions/Domains/Profile/ProfileSynchronizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Functions.Domains.Profile
+{
+    public class ProfileSynchronizer
+    {
+        private readonly HttpClient peopleClient;
+        private readonly HttpClient resourcesClient;
+
+        public ProfileSynchronizer(IHttpClientFactory httpClientFactory)
+        {
+            peopleClient = httpClientFactory.CreateClient(HttpClientNames.Application.People);
+            resourcesClient = httpClientFactory.CreateClient(HttpClientNames.Application.Resources);
+        }
+
+        public async Task SynchronizeAsync()
+        {
+            //retrieve list of personell from resources api
+            //
+
+        }
+    }
+}
---
+++ src/backend/function/Fusion.Resources.Functions/Functions/ProfileSync.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Functions.Functions
+{
+    public class ProfileSync
+    {
+        public ProfileSync()
+        {
+
+        }
+
+        public async Task SyncProfiles([TimerTrigger("* * * * * *", RunOnStartup = true] TimerInfo timer, ILogger log, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
