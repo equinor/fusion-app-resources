c8c8442ab4c29aca64fdb3459440ae5a4467198e
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-03-13 08:25:08 +0100

    Updated request model

--- src/frontend/src/models/PersonnelRequestPosition.ts
+++ src/frontend/src/models/PersonnelRequestPosition.ts
@@ -1,17 +1,25 @@
 import { BasePosition } from '@equinor/fusion';
 
 type PersonnelRequestPosition = {
-    id: string | null;
-    externalId: string | null;
+    id?: string | null;
+    externalId?: string | null;
     basePosition: BasePosition | null;
-    name: string;
+    name: string | null;
     appliesFrom: Date | null;
     appliesTo: Date | null;
     workload: number;
     obs?: string;
-    taskOwner: {
-        id: string;
-    } | null;
+    taskOwner?: {
+        id: string | null;
+    };
+};
+
+export type PersonnelRequestBasePosition = {
+    id: string;
+    name: string | null;
+    discipline: string | null;
+    projectType: string | null;
+    wasResolved: boolean | null;
 };
 
 export default PersonnelRequestPosition;
