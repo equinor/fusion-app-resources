4d2e9b08d3845f853d3a9def8710ecf05225eeca
Eskil Lindløv Sand <eskil.sand@bouvet.no>
2020-03-19 10:21:45 +0100

    Readded request side sheet

--- src/frontend/src/pages/ProjectPage/pages/ContractPage/components/RequestDetailsSideSheet/RequestDetails.tsx
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/components/RequestDetailsSideSheet/RequestDetails.tsx
@@ -26,7 +26,9 @@ const RequestDetails: React.FC<RequestDetailsProps> = ({ request }) => {
 
     return (
         <div className={styles.requestDetails}>
-            <DetailedRequestWorkflow workflow={request.workflow} />
+            <div className={styles.workflow}>
+                <DetailedRequestWorkflow workflow={request.workflow} />
+            </div>
             {createItemField('description', 'Description', () => request.description || 'N/A')}
             {createItemField(
                 'basePosition',
