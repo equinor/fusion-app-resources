61e8d6846f07fa3280de54ab10d915231be6daf6
Ole-Richard Blindheim <post@olerichard.org>
2020-03-20 11:18:50 +0100

    positions tab set up properly

--- src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ManagePersonnelPage/PersonnelInfoSideSheet/PositionsTab/index.tsx
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ManagePersonnelPage/PersonnelInfoSideSheet/PositionsTab/index.tsx
@@ -27,103 +27,45 @@ const mapPositionsToPersoPosition = (p: Position): PersonPosition => {
 };
 
 const PositionsTab: React.FC<PositionsTabProps> = ({ person }) => {
-    const positions = person.positions?.map(mapPositionsToPersoPosition);
 
-    let active: PersonPosition[] = [
-        {
-            id: '37f888d1-dc7b-4eb8-ab12-b5fbb4220b15a',
-            name: 'Administrative Lead',
+    const positions = React.useMemo(() => person.positions?.map(mapPositionsToPersoPosition), [person]);
 
-            obs: 'Project Management Team',
-            project: {
-                id: 'da03f725-29e5-43d9-8f2b-f756873a6034',
-                name: 'Åsgard Subsea Compression Phase II with long text',
-                domainId: '20396',
-                type: '',
-            },
-            basePosition: {
-                id: '3f743b4b-a1e1-414c-8dc9-a3652dab8eb1',
-                name: 'Administration Management',
+    const currentDate = React.useMemo(() => new Date().getTime(), [])
+    const pastPositions = React.useMemo(() => positions?.filter(p => (p.appliesTo?.getTime() || currentDate) <= currentDate), [positions]);
+    const activePositions = React.useMemo(() => positions?.filter(p => ((p.appliesFrom?.getTime() || currentDate) < currentDate) && ((p.appliesTo?.getTime() || currentDate) >= currentDate)), [positions]);
 
-                discipline: 'Quality and Administration',
-            },
-
-            appliesFrom: new Date('2019-10-07T00:00:00'),
-            appliesTo: new Date('2025-06-30T00:00:00'),
-            workload: 25,
-        },
-        {
-            id: '37f888d1-dc7b-4eb8-ab12-b5fbb4220b15b',
-            name: 'Administrative Lead',
-
-            obs: 'Project Management Team',
-            project: {
-                id: 'da03f725-29e5-43d9-8f2b-f756873a6034',
-                name: 'Åsgard Subsea Compression Phase II with long text',
-                domainId: '20396',
-                type: '',
-            },
-            basePosition: {
-                id: '3f743b4b-a1e1-414c-8dc9-a3652dab8eb1',
-                name: 'Administration Management',
-
-                discipline: 'Quality and Administration',
-            },
-
-            appliesFrom: new Date('2018-01-07T00:00:00'),
-            appliesTo: new Date('2019-06-30T00:00:00'),
-            workload: 25,
-        },
-        {
-            id: '37f888d1-dc7b-4eb8-ab12-b5fbb4220b15c',
-            name: 'Administrative Lead',
-
-            obs: 'Project Management Team',
-            project: {
-                id: 'da03f725-29e5-43d9-8f2b-f756873a6034',
-                name: 'Åsgard Subsea Compression Phase II with long text',
-                domainId: '20396',
-                type: '',
-            },
-            basePosition: {
-                id: '3f743b4b-a1e1-414c-8dc9-a3652dab8eb1',
-                name: 'Administration Management',
-
-                discipline: 'Quality and Administration',
-            },
-
-            appliesFrom: new Date('2020-10-07T00:00:00'),
-            appliesTo: new Date('2025-06-30T00:00:00'),
-            workload: 25,
-        },
-    ];
-    const currentDate = Date.parse(Date());
 
     return (
         <div className={styles.container}>
             <div className={styles.activePositions}>
                 <h4>Active Positions</h4>
-                <ul className={styles.positionList}>
-                    {active
-                        ?.filter(p => p.appliesFrom?.getMilliseconds || currentDate < currentDate)
-                        .map(position => (
+                {activePositions?.length ? <ul className={styles.positionList}>
+                    {
+                        activePositions.map(position => (
                             <li key={position.id} className={styles.positionListItem}>
                                 <PersonPositionCard position={position} />
                             </li>
                         ))}
-                </ul>
+                </ul> :
+                    <div className={styles.nopositions}>
+                        No active positions
+                    </div>}
             </div>
             <div className={styles.pastPositions}>
                 <h4>Past Positions</h4>
-                <ul className={styles.positionList}>
-                    {active
-                        ?.filter(p => p.appliesFrom?.getMilliseconds || currentDate < currentDate)
-                        .map(position => (
-                            <li key={position.id} className={styles.positionListItem}>
-                                <PersonPositionCard position={position} />
-                            </li>
-                        ))}
-                </ul>
+
+                {pastPositions?.length ?
+                    <ul className={styles.positionList}>
+                        {
+                            pastPositions.map(position => (
+                                <li key={position.id} className={styles.positionListItem}>
+                                    <PersonPositionCard position={position} />
+                                </li>
+                            ))}
+                    </ul> :
+                    <div className={styles.nopositions}>
+                        No past positions
+                    </div>}
             </div>
         </div>
     );
