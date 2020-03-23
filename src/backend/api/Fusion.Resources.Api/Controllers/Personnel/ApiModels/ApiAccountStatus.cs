5c8805c4397ff305d36024d23725c33f337954f2
Leif Bjarte Johansson <lejoh@equinor.com>
2020-03-23 19:48:33 +0100

    Removing the NotSet account status, undesirable.

--- src/backend/api/Fusion.Resources.Api/Controllers/Personnel/ApiModels/ApiAccountStatus.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/ApiModels/ApiAccountStatus.cs
@@ -1,4 +1,4 @@
 ﻿namespace Fusion.Resources.Api.Controllers
 {
-    public enum ApiAccountStatus { Available, InviteSent, NoAccount, NotSet }
+    public enum ApiAccountStatus { Available, InviteSent, NoAccount }
 }
