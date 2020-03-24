749823d46aa3c8f982e9c52ae95e3d7ca780fb24
Hans Dahle <HANDAH@equinor.com>
2020-03-24 10:06:51 +0100

    Missing migrations

---
+++ src/backend/api/Fusion.Resources.Database/Migrations/20200324060723_Addded linkedin profile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fusion.Resources.Database.Migrations
+{
+    public partial class Adddedlinkedinprofile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LinkedInProfile",
+                table: "ExternalPersonnel",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkedInProfile",
+                table: "ExternalPersonnel");
+        }
+    }
+}
