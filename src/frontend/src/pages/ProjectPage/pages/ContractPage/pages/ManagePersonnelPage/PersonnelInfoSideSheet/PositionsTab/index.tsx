32f1448750453644bc05f692d494849f6fecc67a
Ole-Richard Blindheim <post@olerichard.org>
2020-03-24 12:45:58 +0100

    Person Details overview and edit  (#50)
    * updated Personnel model
    
    * updated manage personnel name and mail fields to open PersonnelInfoSideSheet.
    Added PersonnelSideSheet component.
    
    * configured tabs in personnelInfoSideSheet.
    Built up the generalTab. Pretty much done. Needs larger photo.
    Started on PositionsTab. this is a work in progress
    
    * positions tab set up properly
    
    * Setup edit handling for general tab.
    Still missing save routine
    
    * AddPersonnelSideSheet menu-dots replaced with MoreIcon.
    In both header and on lines.
    save routine added to GeneralTab.
    
    * Moved person change handling from General tab to
    PersonnelInfoSideSheet and implement it using formHook instead.
    
    * save all the files again for vanity reasons
    
    * Some minor bug fixes.
    Adjusted name in filter from AD status to AD. For consistensy with column name.
    When clicking Add button, selected personnel will not be send into sidesheet.
    It just opens with one empty line in stead.
    Edit personnel will still send in selected personnel.
    Giving these two buttons some distinction.
    ToolTip text added to the managepersonnelToolbar buttons.
    
    * Added proper ADStatusIcon.
    Updated AzureAdStatus component using the new icon and proper colors
    
    * small adjustment according to design

---
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ManagePersonnelPage/PersonnelInfoSideSheet/PositionsTab/index.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Personnel, { Position } from '../../../../../../../../models/Personnel';
+import * as styles from './styles.less';
+import { PersonPosition } from '@equinor/fusion';
+import { PersonPositionCard } from '@equinor/fusion-components';
+
+type PositionsTabProps = {
+    person: Personnel;
+};
+
+const mapPositionsToPersoPosition = (p: Position): PersonPosition => {
+    return {
+        id: p.positionId,
+        name: p.name,
+        obs: p.obs,
+        project: {
+            id: p.project.projectId,
+            name: p.project.name,
+            domainId: p.project.domainId,
+            type: p.project.projectType,
+        },
+        basePosition: p.basePosition,
+        appliesFrom: p.appliesFrom,
+        appliesTo: p.appliesTo,
+        workload: p.workload,
+    };
+};
+
+const PositionsTab: React.FC<PositionsTabProps> = ({ person }) => {
+    const positions = React.useMemo(() => person.positions?.map(mapPositionsToPersoPosition), [
+        person,
+    ]);
+
+    const currentDate = React.useMemo(() => new Date().getTime(), []);
+    const pastPositions = React.useMemo(
+        () => positions?.filter(p => (p.appliesTo?.getTime() || currentDate) <= currentDate),
+        [positions]
+    );
+    const activePositions = React.useMemo(
+        () =>
+            positions?.filter(
+                p =>
+                    (p.appliesFrom?.getTime() || currentDate) < currentDate &&
+                    (p.appliesTo?.getTime() || currentDate) >= currentDate
+            ),
+        [positions]
+    );
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.activePositions}>
+                <h4>Active Positions</h4>
+                {activePositions?.length ? (
+                    <ul className={styles.positionList}>
+                        {activePositions.map(position => (
+                            <li key={position.id} className={styles.positionListItem}>
+                                <PersonPositionCard position={position} />
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <div className={styles.nopositions}>No active positions</div>
+                )}
+            </div>
+            <div className={styles.pastPositions}>
+                <h4>Past Positions</h4>
+
+                {pastPositions?.length ? (
+                    <ul className={styles.positionList}>
+                        {pastPositions.map(position => (
+                            <li key={position.id} className={styles.positionListItem}>
+                                <PersonPositionCard position={position} />
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <div className={styles.nopositions}>No past positions</div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PositionsTab;
