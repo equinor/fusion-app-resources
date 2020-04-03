20b115be8237829fb90a94a75d200cafddd9adef
Eskil Lindløv Sand <eslsa@equinor.com>
2020-04-03 10:36:32 +0200

    Updated landing page (#97)

---
+++ src/frontend/src/pages/LandingPage/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as styles from './styles.less';
+import { SortIcon } from '@equinor/fusion-components';
+
+const LandingPage = () => {
+    return (
+        <h2 className={styles.chooseProject}>
+            <SortIcon direction="asc" />
+            <br />
+            Start by choosing project
+        </h2>
+    );
+};
+
+export default LandingPage;
