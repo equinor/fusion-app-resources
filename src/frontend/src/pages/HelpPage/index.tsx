8e41eca03f7d47197e69ada58073ba912175b22f
Eskil Lindløv Sand <eslsa@equinor.com>
2020-04-07 09:59:21 +0200

    Help page (#104)
    * Started on help page
    
    * Finished help p age
    
    * Code review fixes

---
+++ src/frontend/src/pages/HelpPage/index.tsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import * as styles from './styles.less';
+import GeneralFlow from './svg/GeneralFlow';
+import { Tabs, Tab } from '@equinor/fusion-components';
+import { useHistory } from '@equinor/fusion';
+import RequestFlow from './svg/RequestFlow';
+import ContractorAccount from './svg/ContractorAccount';
+import EquinorAccount from './svg/EquinorAccount';
+import SystemAccount from './svg/SystemAccount';
+
+type RoleDescriptionProps = {
+    title: string;
+    icon: React.ReactNode;
+};
+
+const RoleDescription: React.FC<RoleDescriptionProps> = ({ title, icon, children }) => {
+    return (
+        <div className={styles.roleDescription}>
+            <div className={styles.header}>
+                <div>{icon}</div>
+                <span className={styles.title}>{title} </span>
+            </div>
+            <div className={styles.content}>{children}</div>
+        </div>
+    );
+};
+
+const HelpPage: React.FC = () => {
+    const [activeKey, setActiveKey] = React.useState('general-flow');
+    const history = useHistory();
+
+    const onTabKeyChange = React.useCallback(
+        (tabKey: string) => {
+            history.push({
+                pathname: history.location.pathname,
+                search: `${tabKey}`,
+            });
+        },
+        [history.location.pathname]
+    );
+
+    React.useEffect(() => {
+        const searchKey = history.location.search.slice(1);
+        setActiveKey(searchKey);
+    }, [history.location.search]);
+
+    return (
+        <div className={styles.helpContainer}>
+            <Tabs activeTabKey={activeKey} onChange={onTabKeyChange}>
+                <Tab
+                    tabKey="general-flow"
+                    isCurrent={activeKey === 'general-flow'}
+                    title="General flow"
+                >
+                    <div className={styles.tabContainer}>
+                        <div className={styles.rolesContainer}>
+                            <h2>Roles</h2>
+                            <RoleDescription
+                                title="Responsible - procurement"
+                                icon={<ContractorAccount />}
+                            >
+                                The responsible person for planning, coordinating and executing the
+                                procurement process, and with a delegated authority to commit the
+                                company towards suppliers, ensuring right quality and commercial
+                                terms.
+                            </RoleDescription>
+                            <RoleDescription
+                                title="Responsible - company representative (CR)"
+                                icon={<EquinorAccount />}
+                            >
+                                This role acts as the company's single point of contact towards the
+                                contractor/supplier when the agreement has been awarded. Agreement
+                                is a collective term which covers contracts, amendments, purchase
+                                orders and framework agreements. This role acts as the leader of the
+                                agreement team
+                            </RoleDescription>
+                            <RoleDescription title="System account" icon={<SystemAccount />}>
+                                System account is a function that is automatically triggered
+                            </RoleDescription>
+                        </div>
+                        <div className={styles.helpContent}>
+                            <h2 className={styles.title}>General flow</h2>
+                            <GeneralFlow />
+                        </div>
+                    </div>
+                </Tab>
+                <Tab
+                    tabKey="request-flow"
+                    isCurrent={activeKey === 'request-flow'}
+                    title="Request flow"
+                >
+                    <div className={styles.tabContainer}>
+                        <div className={styles.rolesContainer}>
+                            <h2>Roles</h2>
+                            <RoleDescription
+                                title="Contractor and CR./CR"
+                                icon={<ContractorAccount />}
+                            >
+                                Any contractor user can create a request but only the External
+                                company rep. (CR.) or External contract responsible (CR), can
+                                approve, reject or delete one. If rejected it is stored in Completed
+                                requests.
+                            </RoleDescription>
+                            <RoleDescription title="Equinor CR./CR" icon={<EquinorAccount />}>
+                                When the request is approved by the CR. or CR, then an Equinor
+                                employee set as responsible for the contract, either Equinor company
+                                rep or Equinor contract responsible, can approve or reject the
+                                request. If rejected it is stored in Completed requests.
+                            </RoleDescription>
+                            <RoleDescription title="System account" icon={<SystemAccount />}>
+                                System account is a function that is automatically triggered to
+                                provision the request. This will send it to the Pro Org-chart where
+                                it will now be accessible. It will also be stored in the Completed
+                                requests.
+                            </RoleDescription>
+                        </div>
+
+                        <div className={styles.helpContent}>
+                            <h2 className={styles.title}>New and Edit - Request flow</h2>
+                            <RequestFlow />
+                        </div>
+                    </div>
+                </Tab>
+            </Tabs>
+        </div>
+    );
+};
+export default HelpPage;
