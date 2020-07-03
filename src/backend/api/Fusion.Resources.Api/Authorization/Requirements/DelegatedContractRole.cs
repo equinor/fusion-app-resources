d0d68576a8a83f104f1089ef0f91362e5ffe560a
Hans Dahle <handah@equinor.com>
2020-07-03 11:12:48 +0200

    Fixed error thrown when generating description for delegate requirement.

--- src/backend/api/Fusion.Resources.Api/Authorization/Requirements/DelegatedContractRole.cs
+++ src/backend/api/Fusion.Resources.Api/Authorization/Requirements/DelegatedContractRole.cs
@@ -17,13 +17,13 @@ namespace Fusion.Resources.Api.Authorization
             {
                 RoleClassification.Internal => "The user must have been delegated any 'internal' admin role in the contract. This can be done by the internal company rep.",
                 RoleClassification.External => "The user must have been delegated any 'external' admin role in the contract. This can be done by any of the Company reps (internal/external).",
-                _ => throw new NotSupportedException("Unsupported role classification")
+                _ => "The user must have either any 'internal' or 'external' admin role in the contract"
             },
             RoleType.CompanyRep => Classification switch
             {
                 RoleClassification.Internal => "The user must be delegated the CR role for Equinor.",
                 RoleClassification.External => "The user must be delegated the CR role for the external contractor",
-                _ => throw new NotSupportedException("Unsupported role classification")
+                _ => "The user must be delegated the CR role either for 'internal' or 'external' in the contract"                
             },
             _ => throw new NotSupportedException("Invalid role type to generate description for")
         };
