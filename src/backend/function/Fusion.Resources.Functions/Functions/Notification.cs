321ba83bc3b242b4f7b3c6abf2e50103cdf4c05e
Leif Bjarte Johansson <lejoh@equinor.com>
2020-12-15 15:12:45 +0100

    Removed obsolete log stmts.

--- src/backend/function/Fusion.Resources.Functions/Functions/Notification.cs
+++ src/backend/function/Fusion.Resources.Functions/Functions/Notification.cs
@@ -21,24 +21,16 @@ namespace Fusion.Resources.Functions
         [FunctionName("request-summary-notification")]
         public async Task SendRequestSummaryNotification([TimerTrigger("*/5 * * * *", RunOnStartup = false)] TimerInfo timer, ILogger log)
         {
-            log.LogInformation("Request notification summary starting");
-
             await requestNotificationSender.ProcessNotificationsAsync();
-
-            log.LogInformation("Request notification summary completed successfully");
         }
 
         [Singleton]
         [FunctionName("sent-notifications-cleanup")]
         public async Task CleanupSentNotifications([TimerTrigger("0 0 0 * * *", RunOnStartup = true)] TimerInfo timer, ILogger log)
         {
-            log.LogInformation("Notification cleanup started");
-
             var dateCutoff = DateTime.Today.AddDays(-1);
 
             await sentNotificationsTable.CleanupSentNotifications(dateCutoff);
-
-            log.LogInformation("Notification cleanup completed successfully");
         }
     }
 }
