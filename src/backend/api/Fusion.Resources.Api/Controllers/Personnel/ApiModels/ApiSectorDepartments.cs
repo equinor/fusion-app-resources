1a43a58ff475f1b9c0eefc853e2332d698fb0be3
Merethe Heggset <mhegg@equinor.com>
2021-02-17 10:23:13 +0100

    Temporary commit, squash

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/ApiModels/ApiSectorDepartments.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Fusion.Resources.Api.Controllers
+{
+    public class ApiSectorDepartments
+    {
+        public string SectorString { get; set; }
+        //responsible
+        public List<DepartmentInSector> DepartmentsInSector { get; set; }
+    }
+
+    public class DepartmentInSector
+    {
+        public List<ApiInternalPersonnelPerson> DepartmentPersonnel { get; set; }
+        public string DepartmentString { get; set; }
+
+    }
+}
