6c013618d9fc731c6465a1f532c88d313079b499
Merethe Heggset <mhegg@equinor.com>
2021-02-18 16:07:54 +0100

    basic draft for sector personnel endpoint

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/ApiModels/ApiSector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fusion.Resources.Api.Controllers.Personnel.ApiModels
+{
+    public class ApiSector
+    {
+        public ApiSector(string fullDepartmentString)
+        {
+            this.FullDepartmentString = fullDepartmentString;
+        }
+        public string FullDepartmentString { get; set; }
+        public List<SectorDepartment> SectorDepartments { get; set; } = new List<SectorDepartment>();
+        
+        public class SectorDepartment
+        {
+            public SectorDepartment(string fullDepartmentString, List<ApiInternalPersonnelPerson> departmentPersonnel)
+            {
+                this.FullDepartmentString = fullDepartmentString;
+                this.DepartmentPersonnel = departmentPersonnel;
+            }
+            public string FullDepartmentString { get; set; }
+            public List<ApiInternalPersonnelPerson> DepartmentPersonnel { get; set; } = new List<ApiInternalPersonnelPerson>();
+        }
+
+    }
+}
--- src/backend/api/Fusion.Resources.Api/Controllers/Personnel/InternalPersonnelController.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Personnel/InternalPersonnelController.cs
@@ -1,17 +1,23 @@
 ﻿using Fusion.AspNetCore.FluentAuthorization;
 using Fusion.AspNetCore.OData;
 using Fusion.Authorization;
+using Fusion.Integration.Configuration;
 using Fusion.Integration.Http;
+using Fusion.Resources.Api.Controllers.Personnel.ApiModels;
 using Fusion.Resources.Domain;
 using Itenso.TimePeriod;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml;
+using static Fusion.Resources.Api.Controllers.Personnel.ApiModels.ApiSector;
 
 namespace Fusion.Resources.Api.Controllers
 {
@@ -21,8 +27,12 @@ namespace Fusion.Resources.Api.Controllers
     public class InternalPersonnelController : ResourceControllerBase
     {
 
-        public InternalPersonnelController()
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IFusionTokenProvider tokenProvider;
+        public InternalPersonnelController(IHttpClientFactory httpClientFactory, IFusionTokenProvider tokenProvider)
         {
+            this.httpClientFactory = httpClientFactory;
+            this.tokenProvider = tokenProvider;
         }
 
         [HttpGet("departments/{fullDepartmentString}/resources/personnel")]
@@ -90,6 +100,67 @@ namespace Fusion.Resources.Api.Controllers
 
             var returnModel = department.Select(p => new ApiInternalPersonnelPerson(p)).ToList();
             return new ApiCollection<ApiInternalPersonnelPerson>(returnModel);
+        }
+
+        [HttpGet("sectors/{sectorString}/resources/personnel")]
+        public async Task<ActionResult<ApiSector>> GetSectorPersonnel(string sectorString,
+           [FromQuery] ODataQueryParams query,
+           [FromQuery] DateTime? timelineStart = null,
+           [FromQuery] string? timelineDuration = null,
+           [FromQuery] DateTime? timelineEnd = null)
+        {
+            var authResult = await Request.RequireAuthorizationAsync(r =>
+            {
+                r.AnyOf(or =>
+                {
+                    or.BeTrustedApplication();
+                    or.FullControl();
+                    or.FullControlInternal();
+                    // TODO: Figure out auth requirements
+                });
+            });
+
+            if (authResult.Unauthorized)
+                return authResult.CreateForbiddenResponse();
+
+            var apiSector = new ApiSector(sectorString);
+            var departmentStrings = await FindChildDepartments(sectorString);
+            
+            foreach (var departmentString in departmentStrings)
+            {
+                var departmentPersonnel = await DispatchAsync(new GetDepartmentPersonnel(departmentString, query));
+                // .WithTimeline(shouldExpandTimeline, timelineStart, timelineEnd)) ;
+                var apiPersonnel = departmentPersonnel?.Select(p => new ApiInternalPersonnelPerson(p)).ToList();
+                var sectorDepartment = new SectorDepartment(departmentString, apiPersonnel);
+                apiSector.SectorDepartments.Add(sectorDepartment);
+            }
+            return apiSector;
+        }
+
+        private async Task<List<string>> FindChildDepartments(string sectorString)
+        {
+            var sectors = new[] {
+            new { Sector = "TPD PRD FE MMS", Departments = new List<string>{ "TPD PRD FE MMS MAT1",
+                                                                             "TPD PRD FE MMS MAT2",
+                                                                             "TPD PRD FE MMS MEC1",
+                                                                             "TPD PRD FE MMS MEC2",
+                                                                             "TPD PRD FE MMS MEC3",
+                                                                             "TPD PRD FE MMS MEC4",
+                                                                             "TPD PRD FE MMS STR1",
+                                                                             "TPD PRD FE MMS STR2" } },
+            new { Sector = "TPD PRD PMC PCA", Departments = new List<string>{ "TPD PRD PMC PCA PCA1",
+                                                                             "TPD PRD PMC PCA PCA2",
+                                                                             "TPD PRD PMC PCA PCA3",
+                                                                             "TPD PRD PMC PCA PCA4",
+                                                                             "TPD PRD PMC PCA PCA5",
+                                                                             "TPD PRD PMC PCA PCA6",
+                                                                             "TPD PRD PMC PCA PCA7" } }
+            };
+      
+            var departmentStrings = new List<String>();
+            departmentStrings.AddRange(sectors.First(s => s.Sector == sectorString).Departments);
+                                              
+            return departmentStrings;
         }
     }
 
