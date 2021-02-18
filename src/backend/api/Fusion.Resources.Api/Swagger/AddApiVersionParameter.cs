bb0f6003766a4e71b204f07abf39062bc19b6aa4
Hans Dahle <handah@equinor.com>
2021-02-18 16:49:21 +0100

    Added api versioning and updated swagger with improved version and complext query params support.

---
+++ src/backend/api/Fusion.Resources.Api/Swagger/AddApiVersionParameter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class AddApiVersionParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var version = context.ApiDescription.GetApiVersion() ?? new AspNetCore.Mvc.ApiVersion(1, 0);
+
+            var apiVersionParam = new OpenApiParameter()
+            {
+                Name = "api-version",
+                Required = true,
+                In = ParameterLocation.Query
+            };
+            apiVersionParam.Example = new OpenApiString(version.ToString());
+
+            operation.Parameters.Add(apiVersionParam);
+                
+        }
+    }
+}
---
+++ src/backend/api/Fusion.Resources.Api/Swagger/SwaggerApiConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public class SwaggerApiConfig
+    {
+        public string Title { get; set; } = null!;
+        public List<int> EnabledVersions { get; } = new List<int>();
+        public bool EnablePreview { get; set; }
+    }
+}
