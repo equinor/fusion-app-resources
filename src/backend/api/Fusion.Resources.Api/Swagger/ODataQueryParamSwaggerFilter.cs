8d825376a819b62e503c37b552b2c4f87811b360
Hans Dahle <handah@equinor.com>
2021-02-22 11:08:12 +0100

    Api versioning and swagger improvements (#215)
    Added api versioning support with default 1.0. This should let all existing endpoints work as execpted without having to provide a ?api-version= param.
    
    As uasability of swagger degrades when version is added, as the query param is not defined anywhere, we must add support for this by manipulating the output through swagger filters.
    This filter adds a api-version query param to all endpoints and sets default value for a specific document. This means different versions must be added to different documents.
    
    By making pages for different versions the usability of swagger ui improves, however using swagger to generate clients etc could see a 'hit'.
    
    Also added swagger filters to make the complex models used in odata filtering usable, like removing the properties and replacing with a single $filter query string.

---
+++ src/backend/api/Fusion.Resources.Api/Swagger/ODataQueryParamSwaggerFilter.cs
@@ -0,0 +1,51 @@
+﻿using Fusion.AspNetCore.OData;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Replace any params of the type ODataQueryParam, with the $-query params.
+    /// </summary>
+    public class ODataQueryParamSwaggerFilter : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            foreach (var path in swaggerDoc.Paths)
+            {
+                foreach (var op in path.Value.Operations)
+                {
+                    var @params = op.Value.Parameters.ToList();
+
+                    var pathParams = @params.Where(p => p.In == ParameterLocation.Query).ToList();
+
+                    var odataQueryParam = pathParams.Where(p => p.Schema != null && p.Schema.Reference != null)
+                        .FirstOrDefault(p => p.Schema.Reference.Id == nameof(ODataQueryParams));
+
+                    if (odataQueryParam != null)
+                    {
+                        op.Value.Parameters.Remove(odataQueryParam);
+
+                        op.Value.Parameters.Add(new OpenApiParameter
+                        {
+                            Name = "$expand",
+                            In = ParameterLocation.Query,
+                            Description = "Comma seperated list of properties to include",
+                            AllowEmptyValue = true
+                        });
+
+                        op.Value.Parameters.Add(new OpenApiParameter
+                        {
+                            Name = "$filter",
+                            In = ParameterLocation.Query,
+                            Description = "OData query filter",
+                            AllowEmptyValue = true,
+                            AllowReserved = true
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/backend/api/Fusion.Resources.Api/Swagger/ToStringTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Type converter to make asp.net core convert the complext type to a string. 
+    /// This is usefull when the framework explodes complext types in either query or route, when it is in reality a string that is bound by logic.
+    /// 
+    /// Ref:
+    /// https://blog.magnusmontin.net/2020/04/03/custom-data-types-in-asp-net-core-web-apis/
+    /// https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/456
+    /// </summary>
+    public class ToStringTypeConverter : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string);
+        }
+
+
+    }
+}
