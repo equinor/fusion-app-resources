069f4488484b76b581a82aea112867a1de130282
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-02-26 10:25:17 +0100

    migration cleanup

---
+++ src/backend/api/Fusion.Resources.Database/Migrations/20210226091833_Cleanup_OrgPositionInstance.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fusion.Resources.Database.Migrations
+{
+    public partial class Cleanup_OrgPositionInstance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrgPositionInstance_AppliesFrom",
+                table: "ResourceAllocationRequests");
+
+            migrationBuilder.DropColumn(
+                name: "OrgPositionInstance_AppliesTo",
+                table: "ResourceAllocationRequests");
+
+            migrationBuilder.DropColumn(
+                name: "OrgPositionInstance_LocationId",
+                table: "ResourceAllocationRequests");
+
+            migrationBuilder.DropColumn(
+                name: "OrgPositionInstance_Obs",
+                table: "ResourceAllocationRequests");
+
+            migrationBuilder.DropColumn(
+                name: "OrgPositionInstance_Workload",
+                table: "ResourceAllocationRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "OrgPositionInstance_Id",
+                table: "ResourceAllocationRequests",
+                newName: "OrgPositionInstanceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrgPositionInstanceId",
+                table: "ResourceAllocationRequests",
+                newName: "OrgPositionInstance_Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrgPositionInstance_AppliesFrom",
+                table: "ResourceAllocationRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrgPositionInstance_AppliesTo",
+                table: "ResourceAllocationRequests",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrgPositionInstance_LocationId",
+                table: "ResourceAllocationRequests",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrgPositionInstance_Obs",
+                table: "ResourceAllocationRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "OrgPositionInstance_Workload",
+                table: "ResourceAllocationRequests",
+                type: "float",
+                nullable: true);
+        }
+    }
+}
