b81f15f8a47b7ecb4da5f6b4f7459d5e3ac87d16
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-03-02 11:12:00 +0100

    simplify model mapping

---
+++ src/backend/api/Fusion.Resources.Logic/Extensions/JsonSerializerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace Fusion.Resources
+{
+    public static class JsonSerializerExtensions
+    {
+        public static T? DeserializeAnonymousType<T>(string json, T anonymousTypeObject, JsonSerializerOptions options = default!)
+            => JsonSerializer.Deserialize<T>(json, options);
+    }
+}
