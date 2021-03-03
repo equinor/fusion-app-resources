5cdcb9f2bbdfc5c43958f2bab7a4db5d068bfcf9
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-03-03 10:49:15 +0100

    cleanup

--- src/backend/api/Fusion.Resources.Api/Controllers/Requests/ApiModels/ApiTaskOwnerWithPerson.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Requests/ApiModels/ApiTaskOwnerWithPerson.cs
@@ -1,14 +1,16 @@
-﻿using Fusion.Resources.Domain;
+﻿using System;
+using Fusion.Resources.Domain;
 
 namespace Fusion.Resources.Api.Controllers
 {
-    public class ApiTaskOwnerWithPerson : TaskOwnerReference
+    public class ApiTaskOwnerWithPerson 
     {
         public ApiTaskOwnerWithPerson(QueryTaskOwnerWithPerson query)
         {
             this.PositionId = query.PositionId;
             this.Person = ApiPerson.FromEntityOrDefault(query.Person);
         }
+        public Guid? PositionId { get; set; }
         public ApiPerson? Person { get; set; }
     }
 }
