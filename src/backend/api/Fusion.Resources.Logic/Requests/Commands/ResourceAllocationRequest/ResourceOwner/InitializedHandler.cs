5b95d3146c0f87dc351b67dd0bd35662d9533a1d
Hans Dahle <HANDAH@equinor.com>
2021-03-30 21:42:03 +0200

    Refactored validation into abstract validator

--- src/backend/api/Fusion.Resources.Logic/Requests/Commands/ResourceAllocationRequest/ResourceOwner/InitializedHandler.cs
+++ src/backend/api/Fusion.Resources.Logic/Requests/Commands/ResourceAllocationRequest/ResourceOwner/InitializedHandler.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Fusion.Resources.Database;
 using Fusion.Resources.Logic.Workflows;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 
 namespace Fusion.Resources.Logic.Commands
 {
@@ -18,12 +17,10 @@ namespace Fusion.Resources.Logic.Commands
             public class InitializedHandler : INotificationHandler<RequestInitialized>
             {
                 private readonly ResourcesDbContext dbContext;
-                private readonly IMediator mediator;
 
-                public InitializedHandler(ResourcesDbContext dbContext, IMediator mediator)
+                public InitializedHandler(ResourcesDbContext dbContext)
                 {
                     this.dbContext = dbContext;
-                    this.mediator = mediator;
                 }
 
                 public async Task Handle(RequestInitialized notification, CancellationToken cancellationToken)
@@ -39,99 +36,24 @@ namespace Fusion.Resources.Logic.Commands
 
                     ValidateWorkflow(workflow, request);
 
-
                     dbContext.Workflows.Add(workflow.CreateDatabaseEntity(notification.RequestId, DbRequestType.InternalRequest));
 
                     request.LastActivity = DateTime.UtcNow;
                     request.State.State = AllocationDirectWorkflowV1.CREATED;
 
-                    // Try to resolve the default assigned department
                     await dbContext.SaveChangesAsync();
-
-                    //await mediator.Publish(new AllocationRequestStarted(request.Id, workflow));
                 }
-
 
                 private void ValidateWorkflow(ResourceOwnerChangeWorkflowV1 workflow, DbResourceAllocationRequest request)
                 {
-                    var isFutureSplit = request.OrgPositionInstance.AppliesFrom > DateTime.UtcNow.Date;
-                    var isExpiredSplit = request.OrgPositionInstance.AppliesTo < DateTime.UtcNow.Date;
+                    var validator = new RequestValidator();
+                    var result = validator.Validate(request);
 
-                    var hasChanges = !string.IsNullOrEmpty(request.ProposedChanges);
-                    var hasPersonChange = request.ProposedPerson.HasBeenProposed;
-
-                    var hasChangeDate = request.ProposalParameters.ChangeFrom != null || request.ProposalParameters.ChangeTo != null;
-
-
-                    if (isExpiredSplit)
-                        throw InvalidWorkflowError.ValidationError("Cannot create change request on expired instance.")
-                            .SetWorkflowName(workflow);
-
-
-                    var subType = new SubType(request.SubType);
-
-                    switch (subType.Value)
+                    if (!result.IsValid)
                     {
-                        case SubType.Types.Adjustment:
-                            if ((!hasChanges && !hasPersonChange) || (!isFutureSplit && !hasChangeDate))
-                                throw InvalidWorkflowError.ValidationError("Required properties are missing in order to start the workflow.", s =>
-                                {
-                                    var changes = JsonConvert.DeserializeAnonymousType(request.ProposedChanges ?? "{}", new { workload = (double?)null, location = new { } });
-
-                                    var hasAdjustmentChanges = changes.workload is null && changes.location is null;
-
-                                    if (!isFutureSplit && !hasChangeDate)
-                                        s.AddFailure("proposalParameters.changeDateFrom", "When the instance to change is currently active, a date the change is going to take effect is required.");
-
-                                    if  (hasAdjustmentChanges)
-                                        s.AddFailure("changes", "Either proposed changes or proposed person must be set.");
-
-                                }).SetWorkflowName(workflow);
-                            break;
-
-                        case SubType.Types.ChangeResource:
-                            if ((!hasChanges && !hasPersonChange) || (!isFutureSplit && !hasChangeDate))
-                                throw InvalidWorkflowError.ValidationError("Required properties are missing in order to start the workflow.", s =>
-                                {
-                                    if (!isFutureSplit && !hasChangeDate)
-                                        s.AddFailure("proposalParameters.changeDateFrom", "When the instance to change is currently active, a date the change is going to take effect is required.");
-
-                                    // Change request requires that there is defined a person in the proposed changes
-                                    var changes = JsonConvert.DeserializeAnonymousType(request.ProposedChanges ?? "{}", new { assignedPerson = new { AzureUniqueId = Guid.Empty } });
-
-                                    var hasPersonToChangeTo = hasPersonChange;
-                                    //var hasPersonToChangeTo = (changes.assignedPerson?.AzureUniqueId).GetValueOrDefault(Guid.Empty) != Guid.Empty;
-                                    //var hasPersonToChangeFrom = hasPersonChange;
-
-                                    //if (!hasPersonToChangeTo)
-                                    //    s.AddFailure("proposedChanges.assignedPerson.azureUniqueId", "Must specify person to change to");
-
-                                    if (hasPersonToChangeTo)
-                                        s.AddFailure("proposedPersonAzureUniqueId", "Must specify person to change to");
-
-                                }).SetWorkflowName(workflow);
-                            break;
-
-                        case SubType.Types.RemoveResource:
-                            if (!isFutureSplit && !hasChangeDate)
-                                throw InvalidWorkflowError.ValidationError("Required properties are missing in order to start the workflow.", s =>
-                                {
-                                    if (!isFutureSplit && !hasChangeDate)
-                                        s.AddFailure("proposalParameters.changeDateFrom", "When the instance to change is currently active, a date the change is going to take effect is required.");
-
-                                    if (request.OrgPositionInstance.AssignedToUniqueId is null)
-                                        s.AddFailure("instance.assignedPerson", "Must specify person to unassign");
-
-                                    //if (hasPersonChange)
-                                    //    s.AddFailure("proposedPersonAzureUniqueId", "Must specify person to unassign");
-
-                                }).SetWorkflowName(workflow);
-                            break;
-
-                        default:
-                            throw InvalidWorkflowError.ValidationError($"Sub type '{request.SubType}' is not valid")
-                                .SetWorkflowName(workflow);
-                    }                    
+                        throw new InvalidWorkflowError("Required properties are missing in order to start the workflow.", result.Errors)
+                            .SetWorkflowName(workflow);
+                    }
                 }
             }
         }
---
+++ src/backend/api/Fusion.Resources.Logic/Requests/Commands/ResourceAllocationRequest/ResourceOwner/RequestValidator.cs
@@ -0,0 +1,101 @@
+﻿using Fusion.Resources.Database.Entities;
+using System;
+using Newtonsoft.Json;
+using FluentValidation;
+
+namespace Fusion.Resources.Logic.Commands
+{
+    public partial class ResourceAllocationRequest
+    {
+        public partial class ResourceOwner
+        {
+            public class RequestValidator : AbstractValidator<DbResourceAllocationRequest>
+            {
+                private static DateTime Today = DateTime.UtcNow.Date;
+
+                public RequestValidator()
+                {
+                    RuleFor(x => x.SubType).NotNull();
+                    RuleFor(x => x.SubType).IsEnumName(typeof(SubType.Types), false)
+                        .WithMessage($"Subtype must be any of [{string.Join(", ", Enum.GetNames<SubType.Types>())}]");
+                        
+
+                    RuleFor(x => x).SetValidator(new AdjustmentValidator()).When(x => new SubType(x.SubType).Value == SubType.Types.Adjustment);
+                    RuleFor(x => x).SetValidator(new RemoveResourceValidator()).When(x => new SubType(x.SubType).Value == SubType.Types.RemoveResource);
+                    RuleFor(x => x).SetValidator(new ChangeResourceValidator()).When(x => new SubType(x.SubType).Value == SubType.Types.ChangeResource);
+
+                    RuleFor(x => x).Must(x => !IsExpiredSplit(x))
+                        .WithMessage("Cannot run request on an expired instance");
+                        
+                }
+
+
+                #region Sub type validators
+
+                private class AdjustmentValidator : AbstractValidator<DbResourceAllocationRequest>
+                {
+                    public AdjustmentValidator()
+                    {
+                        RuleFor(x => x).Custom((r, ctx) =>
+                        {
+                            var changes = JsonConvert.DeserializeAnonymousType(r.ProposedChanges ?? "{}", new { workload = (double?)null, location = new { } });
+
+                            var missingAdjustmentChanges = changes.workload is null && changes.location is null;
+
+                            // When split is currently active, a change date is required.
+                            if (IsCurrentSplit(r) && HasChangeDate(r) == false)
+                                ctx.AddFailure("proposalParameters.changeDateFrom", "When the instance to change is currently active, a date the change is going to take effect is required.");
+
+                            if (missingAdjustmentChanges)
+                                ctx.AddFailure("changes", "Either proposed changes or proposed person must be set.");
+
+                        });
+                    }
+                }
+
+                private class RemoveResourceValidator : AbstractValidator<DbResourceAllocationRequest>
+                {
+                    public RemoveResourceValidator()
+                    {
+                        RuleFor(x => x.ProposalParameters)
+                            .Must(x => x.ChangeFrom != null || x.ChangeTo != null)
+                            .WithMessage("When the instance to change is currently active, a date the change is going to take effect is required.")
+                            .When(x => IsCurrentSplit(x));
+
+                        RuleFor(x => x.OrgPositionInstance.AssignedToUniqueId)
+                            .NotNull()
+                            .NotEmpty()
+                            .WithMessage("There is no person assigned to the instance. Person might already be removed?");
+                    }
+                }
+
+                private class ChangeResourceValidator : AbstractValidator<DbResourceAllocationRequest>
+                {
+                    public ChangeResourceValidator()
+                    {
+                        RuleFor(x => x.ProposalParameters)
+                            .Must(x => x.ChangeFrom != null || x.ChangeTo != null)
+                            .WithMessage("When the instance to change is currently active, a date the change is going to take effect is required.")
+                            .When(x => IsCurrentSplit(x));
+
+                        RuleFor(x => x.ProposedPerson.AzureUniqueId)
+                            .NotNull()
+                            .NotEmpty()
+                            .WithMessage("Must specify person to change to");
+                    }
+                }
+
+                #endregion
+
+                private static bool HasChangeDate(DbResourceAllocationRequest request)
+                    => request.ProposalParameters.ChangeTo is not null || request.ProposalParameters.ChangeFrom is not null;
+
+                private static bool IsCurrentSplit(DbResourceAllocationRequest request) 
+                    => request.OrgPositionInstance.AppliesFrom.Date <= Today && request.OrgPositionInstance.AppliesTo >= Today;
+
+                private static bool IsExpiredSplit(DbResourceAllocationRequest request) 
+                    => request.OrgPositionInstance.AppliesTo <= Today;
+            }
+        }
+    }
+}
