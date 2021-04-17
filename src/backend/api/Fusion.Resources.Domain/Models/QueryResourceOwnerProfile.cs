89b971d9917b1fcd9a2780a8862d9e7b48869474
Hans Dahle <handah@equinor.com>
2021-04-17 07:09:40 +0200

    Updated to latest integration lib version (#262)
    Updated to latest version of integration lib, with more claims like resource owner etc.
    
    Refactored how the profile is resolved, and also included more department info resolved from the line org.

---
+++ src/backend/api/Fusion.Resources.Domain/Models/QueryResourceOwnerProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fusion.Resources.Domain.Queries
+{
+    public class QueryResourceOwnerProfile
+    {
+        public QueryResourceOwnerProfile(string? fullDepartment, bool isDepartmentManager, IEnumerable<string> departmentsWithResponsibility, IEnumerable<string> relevantSectors)
+        {
+            FullDepartment = fullDepartment;
+            IsDepartmentManager = isDepartmentManager;
+            DepartmentsWithResponsibility = departmentsWithResponsibility.ToList();
+            RelevantSectors = relevantSectors.ToList();
+        }
+
+        public string? FullDepartment { get; set; }
+        public bool IsDepartmentManager { get; set; }
+        public List<string> DepartmentsWithResponsibility { get; set; }
+        public List<string> RelevantSectors { get; set; }
+
+        /// <summary>
+        /// Property resolved from line org, could be null if the integration fails.
+        /// </summary>
+        public List<string>? ChildDepartments { get; set; }
+        /// <summary>
+        /// Property resolved from line org, could be null if the integration fails.
+        /// </summary>
+        public List<string>? SiblingDepartments { get; set; }
+        public string? Sector { get; set; }
+    }
+}
