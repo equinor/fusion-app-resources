29ef6675673f63dddae6dd00992e696c4fe9be3a
Hans Dahle <handah@equinor.com>
2021-04-26 09:43:38 +0200

    Person notes authorization (#274)
    Added authroization to endpoints related to person notes. 
    These are basic rules related to the resource owner flag and how they are in relations to the person the note exists at. 
    
    The direct resource owner is allowed to manage the notes. 
    Shared notes are visible for resource owners within the same L2. Ex. `L1 A DEP1` and `L1 A DEP2` is allowed, but `L1 B *` is not allowed.

---
+++ src/backend/tests/Fusion.Resources.Api.Tests/IntegrationTests/ModelTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Fusion.Testing;
+using Xunit;
+#nullable enable
+namespace Fusion.Resources.Api.Tests.IntegrationTests
+{
+    public class ModelTests
+    {
+
+        [Theory]
+        [InlineData("L1", 1)]
+        [InlineData("L1 L2", 2)]
+        [InlineData("L1 L2 L3", 3)]
+        [InlineData("L1 L2 L3 L4", 4)]
+        [InlineData("L1 L2 L3 L4 L5", 5)]
+        [InlineData("L1 L2 L3 L4 L5 ", 5)]
+        public void DepartmentPath_ShouldIdentifiyCorrectLevel(string path, int expectedLevel)
+        {
+            var dep = new DepartmentPath(path);
+            dep.Level.Should().Be(expectedLevel);
+        }
+
+        [Fact]
+        public void DepartmentPath_ShouldReturnCorrectPath_WhenGoTo()
+        {
+            var dep = new DepartmentPath("L1 L2 L3 L4");
+            dep.GoToLevel(2).Should().Be("L1 L2");
+        }
+
+        [Fact]
+        public void DepartmentPath_ShouldReturnCorrectParent()
+        {
+            var dep = new DepartmentPath("L1 L2 L3 L4");
+            dep.Parent().Should().Be("L1 L2 L3");
+        }
+
+        [Fact]
+        public void DepartmentPath_ShouldReturnCorrectParent_WhenJumpingLevel()
+        {
+            var dep = new DepartmentPath("L1 L2 L3 L4");
+            dep.Parent(2).Should().Be("L1 L2");
+        }
+    }
+}
