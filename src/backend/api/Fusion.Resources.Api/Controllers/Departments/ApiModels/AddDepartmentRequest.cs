e203796fb1329ef09a37271814c5b36f033f3df9
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-04-27 14:35:17 +0200

    add endpoints to add/update departments/sectors

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/AddDepartmentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fusion.Resources.Api.Controllers.Departments
+{
+    public class AddDepartmentRequest
+    {
+        [Required]
+        public string DepartmentId { get; set; } = null!;
+        public string? SectorId { get; set; }
+    }
+}
