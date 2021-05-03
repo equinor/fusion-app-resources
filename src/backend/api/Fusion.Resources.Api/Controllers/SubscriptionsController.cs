0f513fe6f71590895ac9c0fff92f3d867463f38b
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-05-03 13:53:00 +0200

    Simple subscription controller

---
+++ src/backend/api/Fusion.Resources.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,50 @@
+﻿using Fusion.Events;
+using Fusion.Integration;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly IFusionProfileResolver profileResolver;
+        private readonly IApiSubscriptionManager subscriptionManager;
+
+        public SubscriptionsController(IFusionProfileResolver profileResolver, IApiSubscriptionManager subscriptionManager)
+        {
+            this.profileResolver = profileResolver;
+            this.subscriptionManager = subscriptionManager;
+        }
+
+        [HttpPut("/subscriptions/internal-requests")]
+        public async Task<IActionResult> RenewSubscription([FromBody] SubscriptionRequestV1 request)
+        {
+            if (!User.IsApplicationUser())
+            {
+                return FusionApiError.Forbidden("Only applications can register subscriptions");
+            }
+
+            var appId = User.GetApplicationId();
+            var userId = User.GetAzureUniqueIdOrThrow();
+
+            var servicePrincipal = await profileResolver.ResolveServicePrincipalAsync(userId);
+
+            if (!Enum.TryParse($"{request.Type}", out SubscriptionDetails.SubscriptionType subscriptionType))
+                return FusionApiError.InvalidOperation("InvalidArgument", "The type is not valid.");
+
+            var details = new SubscriptionDetails(subscriptionType, request.Identifier, appId, servicePrincipal!.DisplayName, request.Id);
+            if (request.TypeFilter != null && request.TypeFilter.Length > 0)
+                details.TypeFilter = request.TypeFilter;
+
+
+            var connectionDetails = await subscriptionManager.EnsureSubscriptionAsync(details);
+
+            return new OkObjectResult(new ApiEventSubscriptionV1(connectionDetails, "resources-sub"));
+        }
+    }
+
+ 
+}
