190c82c632474168673496a794e41d58f772eef5
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-05-27 08:13:47 +0200

    fix migration

--- src/backend/api/Fusion.Resources.Database/Migrations/20210526142847_add_task_details_to_absence.cs
+++ src/backend/api/Fusion.Resources.Database/Migrations/20210526142847_add_task_details_to_absence.cs
@@ -37,48 +37,10 @@ namespace Fusion.Resources.Database.Migrations
                 table: "PersonAbsences",
                 type: "nvarchar(max)",
                 nullable: true);
-
-            migrationBuilder.CreateTable(
-                name: "PersonNotes",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    AzureUniqueId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
-                    Content = table.Column<string>(type: "nvarchar(2500)", maxLength: 2500, nullable: true),
-                    IsShared = table.Column<bool>(type: "bit", nullable: false),
-                    Updated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
-                    UpdatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PersonNotes", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_PersonNotes_Persons_UpdatedById",
-                        column: x => x.UpdatedById,
-                        principalTable: "Persons",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PersonNotes_AzureUniqueId",
-                table: "PersonNotes",
-                column: "AzureUniqueId")
-                .Annotation("SqlServer:Clustered", false)
-                .Annotation("SqlServer:Include", new[] { "Id", "Title", "Content", "IsShared", "Updated", "UpdatedById" });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_PersonNotes_UpdatedById",
-                table: "PersonNotes",
-                column: "UpdatedById");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "PersonNotes");
-
             migrationBuilder.DropColumn(
                 name: "IsPrivate",
                 table: "PersonAbsences");
