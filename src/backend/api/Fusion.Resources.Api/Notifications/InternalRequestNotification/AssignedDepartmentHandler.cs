e16af69364b0cdf890c9c03a1296b06ee6705b6d
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-06-22 19:43:21 +0200

    move handler code from domain to api lib

---
+++ src/backend/api/Fusion.Resources.Api/Notifications/InternalRequestNotification/AssignedDepartmentHandler.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Fusion.ApiClients.Org;
+using Fusion.Integration;
+using Fusion.Integration.Notification;
+using Fusion.Integration.Org;
+using Fusion.Resources.Domain;
+using Fusion.Resources.Domain.Commands;
+using Fusion.Resources.Domain.Notifications.InternalRequests;
+using Fusion.Resources.Domain.Queries;
+using MediatR;
+
+namespace Fusion.Resources.Api.Notifications
+{
+
+    public partial class InternalRequestNotification
+    {
+        public class AssignedDepartmentHandler : INotificationHandler<InternalRequestNotifications.AssignedDepartment>
+        {
+            private readonly IMediator mediator;
+            private readonly INotificationBuilder notificationBuilder;
+            private readonly IProjectOrgResolver orgResolver;
+            private readonly IFusionContextResolver contextResolver;
+
+            public AssignedDepartmentHandler(IMediator mediator, INotificationBuilderFactory notificationClient, IProjectOrgResolver orgResolver, IFusionContextResolver contextResolver)
+            {
+                this.mediator = mediator;
+                this.notificationBuilder = notificationClient.CreateDesigner();
+                this.orgResolver = orgResolver;
+                this.contextResolver = contextResolver;
+            }
+
+            public async Task Handle(InternalRequestNotifications.AssignedDepartment notification, CancellationToken cancellationToken)
+            {
+                var request = await GetResolvedOrgData(notification.RequestId);
+
+                if (string.IsNullOrEmpty(request.AllocationRequest.AssignedDepartment))
+                    return;
+
+                notificationBuilder.AddTitle("A personnel request has been assigned to you")
+                    .AddTextBlock("Request created by")
+                    .TryAddProfileCard(request.AllocationRequest.CreatedBy.AzureUniqueId)
+
+                    .AddTextBlockIf("Proposed resource", request.Instance.AssignedPerson != null)
+                    .TryAddProfileCard(request.Instance.AssignedPerson?.AzureUniqueId)
+
+                    .AddDescription("Please review and handle request")
+
+                    .AddFacts(facts => facts
+                        .AddFact("Project", request.Position.Project.Name)
+                        .AddFact("Position", request.Position.Name)
+                        .AddFact("Period",
+                            $"{request.Instance.AppliesFrom:dd.MM.yyyy} - {request.Instance.AppliesTo:dd.MM.yyyy}")
+                        .AddFact("Workload", $"{request.Instance.Workload}")
+                    )
+                    .TryAddOpenPortalUrlAction("Open request", $"{request.PersonnelAllocationPortalUrl}")
+                    .TryAddOpenPortalUrlAction("Open position in org chart", $"{request.OrgAdminPortalUrl}")
+                    ;
+
+                var card = await notificationBuilder.BuildCardAsync();
+
+                await mediator.Send(new NotifyResourceOwner(request.AllocationRequest.AssignedDepartment, card));
+                //var jsonRep = card.ToJson(); // Json can be viewed using https://adaptivecards.io/designer/
+            }
+
+            private async Task<NotificationRequestData> GetResolvedOrgData(Guid requestId)
+            {
+                var internalRequest = await GetInternalRequestAsync(requestId);
+                if (internalRequest is null)
+                    throw new InvalidOperationException($"Internal request {requestId} not found");
+
+                var orgPosition = await orgResolver.ResolvePositionAsync(internalRequest.OrgPositionId.GetValueOrDefault());
+                if (orgPosition == null)
+                    throw new InvalidOperationException(
+                        $"Cannot resolve position for request {internalRequest.RequestId}");
+
+                var orgPositionInstance = orgPosition.Instances.SingleOrDefault(x => x.Id == internalRequest.OrgPositionInstanceId);
+                if (orgPositionInstance == null)
+                    throw new InvalidOperationException(
+                        $"Cannot resolve position instance for request {internalRequest.RequestId}");
+
+                var context = await contextResolver.ResolveContextAsync(ContextIdentifier.FromExternalId(internalRequest.Project.OrgProjectId), FusionContextType.OrgChart);
+                var orgContextId = $"{context?.Id}";
+
+                return new NotificationRequestData(internalRequest, orgPosition, orgPositionInstance)
+                    .WithContextId(orgContextId)
+                    .WithPortalActionUrls();
+            }
+
+            private async Task<QueryResourceAllocationRequest?> GetInternalRequestAsync(Guid requestId)
+            {
+                var query = new GetResourceAllocationRequestItem(requestId).ExpandTaskOwner();
+                var request = await mediator.Send(query);
+                return request;
+            }
+
+            private class NotificationRequestData
+            {
+                public NotificationRequestData(QueryResourceAllocationRequest allocationRequest, ApiPositionV2 position,
+                    ApiPositionInstanceV2 instance)
+                {
+                    AllocationRequest = allocationRequest;
+                    Position = position;
+                    Instance = instance;
+                }
+
+                private string? OrgContextId { get; set; }
+                public QueryResourceAllocationRequest AllocationRequest { get; }
+                public ApiPositionV2 Position { get; }
+                public ApiPositionInstanceV2 Instance { get; }
+                public string? OrgAdminPortalUrl { get; private set; }
+                public string? PersonnelAllocationPortalUrl { get; private set; }
+
+                public NotificationRequestData WithContextId(string? contextId)
+                {
+                    OrgContextId = contextId;
+                    return this;
+                }
+
+                public NotificationRequestData WithPortalActionUrls()
+                {
+                    if (!string.IsNullOrEmpty(OrgContextId))
+                    {
+                        OrgAdminPortalUrl = $"/apps/org-admin/{OrgContextId}/timeline?instanceId={Instance.Id}&positionId={Position.Id}";
+                    }
+
+                    PersonnelAllocationPortalUrl = $"/apps/personnel-allocation/my-requests/resource/request/{AllocationRequest.RequestId}";
+                    return this;
+                }
+            }
+        }
+    }
+}
--- src/backend/api/Fusion.Resources.Domain/Commands/Requests/InternalRequests/NotifyResourceOwner.cs
+++ src/backend/api/Fusion.Resources.Domain/Commands/Requests/InternalRequests/NotifyResourceOwner.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AdaptiveCards;
-using Fusion.Integration.Notification;
-using MediatR;
+﻿using AdaptiveCards;
 
 namespace Fusion.Resources.Domain.Commands
 {
@@ -20,48 +13,5 @@ namespace Fusion.Resources.Domain.Commands
         public string AssignedDepartment{ get; }
         public AdaptiveCard Card { get; }
 
-    }
-
-    public class Handler : AsyncRequestHandler<NotifyResourceOwner>
-    {
-        private readonly IFusionNotificationClient notificationClient;
-        private readonly IMediator mediator;
-
-        public Handler(IFusionNotificationClient notificationClient, IMediator mediator)
-        {
-            this.notificationClient = notificationClient;
-            this.mediator = mediator;
-        }
-        protected override async Task Handle(NotifyResourceOwner request, CancellationToken cancellationToken)
-        {
-            var recipients = await GenerateRecipientsAsync(request.Editor.Person.AzureUniqueId, request.AssignedDepartment);
-            
-            foreach (var recipient in recipients)
-            {
-                await notificationClient.CreateNotificationForUserAsync(recipient, "A personnel request has been assigned to you", request.Card);
-            }
-        }
-
-        private async Task<IEnumerable<Guid>> GenerateRecipientsAsync(Guid notificationInitiatedByAzureUniqueId, string? assignedDepartment)
-        {
-            var recipients = new List<Guid>();
-
-            if (string.IsNullOrEmpty(assignedDepartment))
-                return recipients;
-
-            var ro = await mediator.Send(new GetDepartment(assignedDepartment).ExpandDelegatedResourceOwners());
-            var relevantProfiles = new List<Guid?>();
-            if (ro?.LineOrgResponsible?.AzureUniqueId != null)
-                relevantProfiles.Add(ro.LineOrgResponsible.AzureUniqueId);
-
-            if (ro?.DelegatedResourceOwners != null)
-                relevantProfiles.AddRange(ro.DelegatedResourceOwners.Select(x => x.AzureUniqueId));
-
-            recipients.AddRange(from azureUniqueId in relevantProfiles.Where(x => x.HasValue).Distinct()
-                where azureUniqueId.Value != notificationInitiatedByAzureUniqueId
-                select azureUniqueId.Value);
-
-            return recipients.Distinct();// A person may be a have multiple roles.
-        }
     }
 }
