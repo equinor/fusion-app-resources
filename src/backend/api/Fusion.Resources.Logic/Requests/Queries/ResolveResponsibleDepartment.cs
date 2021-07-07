9397cce25b1319b19c5b00e1ff26ea161440c276
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-07-07 14:46:27 +0200

    fallback to base position when routing requests

--- src/backend/api/Fusion.Resources.Logic/Requests/Queries/ResolveResponsibleDepartment.cs
+++ src/backend/api/Fusion.Resources.Logic/Requests/Queries/ResolveResponsibleDepartment.cs
@@ -1,4 +1,5 @@
-﻿using Fusion.Resources.Database;
+﻿using Fusion.Integration.Org;
+using Fusion.Resources.Database;
 using Fusion.Resources.Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -21,17 +22,21 @@ namespace Fusion.Resources.Logic.Queries
         public class Handler : IRequestHandler<ResolveResponsibleDepartment, string?>
         {
             private readonly ResourcesDbContext resourcesDb;
+            private readonly IProjectOrgResolver orgResolver;
+            private readonly IMediator mediator;
 
-            public Handler(ResourcesDbContext resourcesDb)
+            public Handler(ResourcesDbContext resourcesDb, IProjectOrgResolver orgResolver, IMediator mediator)
             {
                 this.resourcesDb = resourcesDb;
+                this.orgResolver = orgResolver;
+                this.mediator = mediator;
             }
 
             public async Task<string?> Handle(ResolveResponsibleDepartment request, CancellationToken cancellationToken)
             {
                 var dbRequest = await resourcesDb.ResourceAllocationRequests.FirstAsync(r => r.Id == request.RequestId, cancellationToken);
 
-                return await new RequestRouter(resourcesDb).RouteAsync(dbRequest, cancellationToken);
+                return await new RequestRouter(resourcesDb, orgResolver, mediator).RouteAsync(dbRequest, cancellationToken);
             }
         }
     }
