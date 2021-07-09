0cea1554f7f3b7b9604e41efcec2d82daeffd1ec
Svein Inge Carlsen <epostkasse25@hotmail.com>
2021-07-09 11:43:16 +0200

    New period formatting in adaptive cards

---
+++ src/backend/api/Fusion.Resources.Api/Extensions/ApiPositionInstanceV2Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Fusion.ApiClients.Org;
+
+namespace Fusion.Resources.Api
+{
+    public static class ApiPositionInstanceV2Extensions
+    {
+        public static string GetFormattedPeriodString(this ApiPositionInstanceV2 instance)
+        {
+            var months = GetMonths(instance);
+            return $"🗓{instance.AppliesFrom:dd/MM/yy} to 🗓{instance.AppliesTo:dd/MM/yy} ({months} mths) [{instance.Workload}%]";
+        }
+
+        private static int GetMonths(ApiPositionInstanceV2 instance)
+        {
+            int monthsApart = 12 * (instance.AppliesFrom.Year - instance.AppliesTo.Year) + instance.AppliesFrom.Month - instance.AppliesTo.Month;
+            return Math.Abs(monthsApart);
+        }
+    }
+}
--- src/backend/api/Fusion.Resources.Api/Notifications/InternalRequestNotification/RequestInitializedHandler.cs
+++ src/backend/api/Fusion.Resources.Api/Notifications/InternalRequestNotification/RequestInitializedHandler.cs
@@ -56,7 +56,7 @@ namespace Fusion.Resources.Api.Notifications
                                 .AddFactIf("Project", request.Position?.Project?.Name ?? "", request.Position?.Project is not null)
                                 .AddFactIf("Position id", request.Position?.ExternalId ?? "", request.Position?.ExternalId is not null)
                                 .AddFactIf("Position", request.Position?.Name ?? "", request.Position?.Name is not null)
-                                .AddFact("Period", $"{notificationBuilder.Utils.FormatDateString(request.Instance?.AppliesFrom)} - {notificationBuilder.Utils.FormatDateString(request.Instance?.AppliesTo)}")
+                                .AddFact("Period", $"{request.Instance.GetFormattedPeriodString()}")
                                 .AddFact("Workload", $"{request.Instance?.Workload}")
                             )
                             .AddTextBlock($"Created by: {request.AllocationRequest.CreatedBy.Name}")
