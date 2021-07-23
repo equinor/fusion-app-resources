edb0db90246f2534df590af66ac710f9bdd9d117
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-07-23 16:17:01 +0200

    remove unused code

--- src/backend/api/Fusion.Resources.Domain/Utils/DepartmentPath.cs
+++ src/backend/api/Fusion.Resources.Domain/Utils/DepartmentPath.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Fusion.Resources.Domain
 {
@@ -25,17 +24,5 @@ namespace Fusion.Resources.Domain
 
         public string Parent(int levelsToJump = 1) => string.Join(" ", path.SkipLast(levelsToJump));
         public string GoToLevel(int level) => string.Join(" ", path.Take(level <= 0 ? 1 : level));
-
-        public DepartmentPath Intersect(DepartmentPath other)
-        {
-            var intersection = path.Zip(other.path)
-                .TakeWhile(t => t.First.Equals(t.Second, StringComparison.OrdinalIgnoreCase))
-                .Select(t => t.First);
-
-            return new DepartmentPath(string.Join(" ", intersection));
-        }
-
-        public static bool IsPrefix(DepartmentPath source, DepartmentPath prefix)
-            => source.Intersect(prefix).Level > 0;
     }
 }
