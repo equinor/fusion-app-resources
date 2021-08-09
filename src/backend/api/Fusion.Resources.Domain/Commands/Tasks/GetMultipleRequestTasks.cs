6f1b92b2ab9458be4a1cecf3a2be1a77d44421ca
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-08-09 15:32:37 +0200

    add expands to endpoitns for multiple requests

---
+++ src/backend/api/Fusion.Resources.Domain/Commands/Tasks/GetMultipleRequestTasks.cs
@@ -0,0 +1,41 @@
+﻿using Fusion.Resources.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Domain.Commands.Tasks
+{
+    public class GetMultipleRequestTasks : IRequest<ILookup<Guid, QueryRequestTask>>
+    {
+        private IEnumerable<Guid> requestId;
+
+        public GetMultipleRequestTasks(IEnumerable<Guid> requestId)
+        {
+            this.requestId = requestId;
+        }
+
+        public class Handler : IRequestHandler<GetMultipleRequestTasks, ILookup<Guid, QueryRequestTask>>
+        {
+            private readonly ResourcesDbContext db;
+
+            public Handler(ResourcesDbContext db)
+            {
+                this.db = db;
+            }
+            public async Task<ILookup<Guid, QueryRequestTask>> Handle(GetMultipleRequestTasks request, CancellationToken cancellationToken)
+            {
+                var result = await db.RequestTasks
+                    .Include(t => t.ResolvedBy)
+                    .Where(t => request.requestId.Contains(t.RequestId))
+                    .ToListAsync(cancellationToken);
+
+                return result
+                    .ToLookup(x => x.RequestId, x => new QueryRequestTask(x));
+            }
+        }
+    }
+}
