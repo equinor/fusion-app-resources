c271adf5d6fb085a33c35a39dfeb2ef400f87220
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2021-08-19 14:54:50 +0200

    add filter by proposed person to request endpoints

--- src/backend/api/Fusion.Resources.Domain/Queries/GetResourceAllocationRequests.cs
+++ src/backend/api/Fusion.Resources.Domain/Queries/GetResourceAllocationRequests.cs
@@ -174,6 +174,7 @@ namespace Fusion.Resources.Domain.Queries
                         m.MapField("state", i => i.State.State);
                         m.MapField("state.isComplete", i => i.State.IsCompleted);
                         m.MapField("provisioningStatus.state", i => i.ProvisioningStatus.State);
+                        m.MapField(nameof(QueryResourceAllocationRequest.ProposedPerson), x => x.ProposedPerson.AzureUniqueId);
                     });
                 }
 
