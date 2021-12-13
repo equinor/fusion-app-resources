51de03f27603a72f90440c5de507b6df90f8ec01
Eskil Lindløv Sand <eslsa@equinor.com>
2021-12-13 13:11:43 +0100

    fix: table scroll fix and overdu icon

--- src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ActualMppPage/columns.tsx
+++ src/frontend/src/pages/ProjectPage/pages/ContractPage/pages/ActualMppPage/columns.tsx
@@ -1,15 +1,25 @@
-import { DataTableColumn, PersonCard } from '@equinor/fusion-components';
-
+import {
+    DataTableColumn,
+    PersonCard,
+    useTooltipRef,
+    WarningIcon,
+} from '@equinor/fusion-components';
 import PositionColumn from '../../../../components/PositionColumn';
-import { Position, useHistory } from '@equinor/fusion';
+import { formatDate, Position, useHistory } from '@equinor/fusion';
 import styles from './styles.less';
 import { FC, useCallback } from 'react';
+import classNames from 'classnames';
 
 type AssignedPersonProps = {
     item: Position;
 };
+
+type ToDateProp = {
+    appliesTo: Date | undefined;
+};
+
 const AssignedPersonComponent: FC<AssignedPersonProps> = ({ item }) => {
-    const person = item.instances.find(i => i.assignedPerson)?.assignedPerson || undefined;
+    const person = item.instances.find((i) => i.assignedPerson)?.assignedPerson || undefined;
     return <PersonCard person={person} photoSize="medium" inline />;
 };
 
@@ -35,9 +45,39 @@ const ColumnSideSheetLink: FC<ColumnSideSheetLinkProps> = ({ positionId, childre
     );
 };
 
+const ToDateComponent: FC<ToDateProp> = ({ appliesTo }) => {
+    const today = new Date();
+    const isOverdue = appliesTo && appliesTo.getTime() < today.getTime();
+
+    const isSoonDue =
+        appliesTo && new Date(today.setMonth(today.getMonth() + 1)).getTime() > appliesTo.getTime();
+
+    const tooltipContent = isOverdue ? 'Position is overdue' : `Position is soon due`;
+    const tooltipRef = useTooltipRef(tooltipContent, 'left');
+
+    if (!appliesTo) {
+        return <span>No date</span>;
+    }
+
+    const appliesToClasses = classNames(styles.appliesTo, {
+        [styles.isSoonDue]: isSoonDue,
+        [styles.isOverdue]: isOverdue,
+    });
+    return (
+        <div className={appliesToClasses}>
+            <span className={styles.date}>{formatDate(appliesTo)}</span>
+            {(isOverdue || isSoonDue) && (
+                <div className={styles.icon} ref={tooltipRef}>
+                    <WarningIcon outline={false} />
+                </div>
+            )}
+        </div>
+    );
+};
+
 const columns: DataTableColumn<Position>[] = [
     {
-        accessor: position => position.name || 'TBN',
+        accessor: (position) => position.name || 'TBN',
         key: 'position',
         label: 'Position',
         sortable: true,
@@ -46,26 +86,42 @@ const columns: DataTableColumn<Position>[] = [
         ),
     },
     {
-        accessor: position =>
-            position.instances.find(i => i.assignedPerson?.name)?.assignedPerson?.name || '',
+        accessor: (position) =>
+            position.instances.find((i) => i.assignedPerson?.name)?.assignedPerson?.name || '',
         key: 'person',
         label: 'Person',
         sortable: true,
         component: AssignedPersonComponent,
     },
     {
-        accessor: position => position.basePosition?.name || 'TBN',
-        key: 'basePosition',
-        label: 'Base position',
+        accessor: (position) =>
+            position.instances.find((i) => !isNaN(i.workload))?.workload.toString() + '%' || '',
+        key: 'workload',
+        label: 'Workload',
         sortable: true,
         component: ({ item }) => (
             <ColumnSideSheetLink positionId={item.id}>
-                {item.basePosition?.name || 'TBN'}
+                {item.instances.find((i) => !isNaN(i.workload))?.workload.toString() + '%' || ''}
             </ColumnSideSheetLink>
         ),
     },
     {
-        accessor: position => position.basePosition?.discipline || 'TBN',
+        accessor: (position) =>
+            position.instances
+                .find((i) => i.appliesTo)
+                ?.appliesTo.getTime()
+                .toString() || '0',
+        key: 'to-date',
+        label: 'To date',
+        sortable: true,
+        component: ({ item }) => (
+            <ColumnSideSheetLink positionId={item.id}>
+                <ToDateComponent appliesTo={item.instances.find((i) => i.appliesTo)?.appliesTo} />
+            </ColumnSideSheetLink>
+        ),
+    },
+    {
+        accessor: (position) => position.basePosition?.discipline || 'TBN',
         key: 'discipline',
         label: 'Discipline',
         sortable: true,
@@ -77,28 +133,16 @@ const columns: DataTableColumn<Position>[] = [
     },
 
     {
-        accessor: position =>
-            position.instances.find(i => i.parentPositionId)?.parentPositionId || '',
+        accessor: (position) =>
+            position.instances.find((i) => i.parentPositionId)?.parentPositionId || '',
         key: 'taskOwnerId',
         label: 'Task owner',
         sortable: true,
         component: ({ item }) => {
             const taskOwnerId =
-                item.instances.find(i => i.parentPositionId)?.parentPositionId || null;
+                item.instances.find((i) => i.parentPositionId)?.parentPositionId || null;
             return <PositionColumn positionId={taskOwnerId} />;
         },
-    },
-    {
-        accessor: position =>
-            position.instances.find(i => !isNaN(i.workload))?.workload.toString() + '%' || '',
-        key: 'workload',
-        label: 'Workload',
-        sortable: true,
-        component: ({ item }) => (
-            <ColumnSideSheetLink positionId={item.id}>
-                {item.instances.find(i => !isNaN(i.workload))?.workload.toString() + '%' || ''}
-            </ColumnSideSheetLink>
-        ),
     },
 ];
 
