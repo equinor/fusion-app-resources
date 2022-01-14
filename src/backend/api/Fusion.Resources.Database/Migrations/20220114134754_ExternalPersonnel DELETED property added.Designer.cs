be8e1917cf30b13de853ca797032849f282cd186
Svein Inge Carlsen <epostkasse25@hotmail.com>
2022-01-14 14:48:43 +0100

    Add DELETED property to external personnel table

--- src/backend/api/Fusion.Resources.Database/Migrations/20220114134754_ExternalPersonnel DELETED property added.Designer.cs
+++ src/backend/api/Fusion.Resources.Database/Migrations/20220114134754_ExternalPersonnel DELETED property added.Designer.cs
@@ -4,14 +4,16 @@ using Fusion.Resources.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fusion.Resources.Database.Migrations
 {
     [DbContext(typeof(ResourcesDbContext))]
-    partial class ResourcesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220114134754_ExternalPersonnel DELETED property added")]
+    partial class ExternalPersonnelDELETEDpropertyadded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
