b1b7cfd96b608e0fdc9789d6bdb47d44961a3e91
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2022-01-20 10:08:40 +0100

    delete actions when request is provisioned

---
+++ src/backend/api/Fusion.Resources.Logic/DeleteActionsHandler.cs
@@ -0,0 +1,29 @@
+﻿using Fusion.Resources.Database;
+using Fusion.Resources.Logic.Events;
+using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class DeleteActionsHandler : INotificationHandler<RequestProvisioned>
+{
+    private readonly ResourcesDbContext resourcesDb;
+
+    public DeleteActionsHandler(ResourcesDbContext resourcesDb)
+    {
+        this.resourcesDb = resourcesDb;
+    }
+    public async Task Handle(RequestProvisioned notification, CancellationToken cancellationToken)
+    {
+        await DeleteActions(notification.RequestId, cancellationToken);
+    }
+
+    private Task DeleteActions(Guid requestId, CancellationToken cancellationToken)
+    {
+        resourcesDb.RequestActions.RemoveRange(
+            resourcesDb.RequestActions.Where(c => c.RequestId == requestId)
+        );
+        return resourcesDb.SaveChangesAsync(cancellationToken);
+    }
+}
