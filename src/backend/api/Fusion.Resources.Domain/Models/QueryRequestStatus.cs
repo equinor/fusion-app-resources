a47ba08e3769fb8e82d14f60d7863761b1313449
Lorentz Vedeler <lorentz.vedeler@bouvet.no>
2022-01-27 15:41:01 +0100

    Add `ChangeRequestStatus` to Person Position Timeline (#454)
    * Add `ChangeRequestStatus` to person posiiton item
    
    * hardcode date
    
    * Revert "hardcode date"
    
    This reverts commit 4303a88978878cde244ff455cc25b3c112bd55cb.
    
    * ensure position does not interfere with other tests when run in parallel

---
+++ src/backend/api/Fusion.Resources.Domain/Models/QueryRequestStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fusion.Resources.Domain.Models
+{
+    public class QueryRequestStatus
+    {
+        public QueryRequestStatus(QueryResourceAllocationRequest changeRequest)
+        {
+            Id = changeRequest.RequestId;
+            State = changeRequest.State;
+            IsDraft = changeRequest.IsDraft;
+        }
+
+        public Guid Id { get; }
+        public string? State { get; }
+        public bool IsDraft { get; } = false;
+    }
+}
