faca1ef8f8b89650886d8832acbceaa6abd60342
Svein Inge Carlsen <epostkasse25@hotmail.com>
2022-02-09 11:03:32 +0100

    Migration to set IsDeleted flag null on table ExternalPersonnel

---
+++ src/backend/api/Fusion.Resources.Database/Migrations/20220209100249_ExternalPersonnelIsDeletedNull.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fusion.Resources.Database.Migrations
+{
+    public partial class ExternalPersonnelIsDeletedNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "ExternalPersonnel",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "ExternalPersonnel",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
