f0a0566cb9c3d1cc6e0fc9c9a195c2e9ec1ee640
Lorentz Vedeler <lrvedeler@gmail.com>
2022-05-13 13:00:00 +0200

    add data model

--- src/backend/api/Fusion.Resources.Database/Migrations/20220516095453_add_shared_requests.Designer.cs
+++ src/backend/api/Fusion.Resources.Database/Migrations/20220516095453_add_shared_requests.Designer.cs
@@ -4,14 +4,16 @@ using Fusion.Resources.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fusion.Resources.Database.Migrations
 {
     [DbContext(typeof(ResourcesDbContext))]
-    partial class ResourcesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220516095453_add_shared_requests")]
+    partial class add_shared_requests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/backend/api/Fusion.Resources.Database/Migrations/20220516095453_add_shared_requests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fusion.Resources.Database.Migrations
+{
+    public partial class add_shared_requests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SharedRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RequestId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SharedWithId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SharedById = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Scope = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    IsRevoked = table.Column<bool>(type: "bit", nullable: false),
+                    RevokedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    GrantedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SharedRequests_Persons_SharedById",
+                        column: x => x.SharedById,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SharedRequests_Persons_SharedWithId",
+                        column: x => x.SharedWithId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SharedRequests_ResourceAllocationRequests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "ResourceAllocationRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedRequests_RequestId",
+                table: "SharedRequests",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedRequests_SharedById",
+                table: "SharedRequests",
+                column: "SharedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedRequests_SharedWithId",
+                table: "SharedRequests",
+                column: "SharedWithId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharedRequests");
+        }
+    }
+}
