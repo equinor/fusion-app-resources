12585092f841cf1f50564d8de6f061bad9b0ce3e
Svein Inge Carlsen <epostkasse25@hotmail.com>
2022-05-19 13:56:11 +0200

    Pro-Org - Positions having allocation request missing in org-chart histogram (#493)
    * Update OrgPositionInstance hasRequest property when creating or deleting allocation request
    
    * use notification instead of controller to indicate hasRequest property

---
+++ src/backend/api/Fusion.Resources.Api/FusionEvents/Handlers/InternalRequests/UpdateOrgPositionInstanceHaveRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Fusion.Resources.Domain.Notifications.InternalRequests;
+using Fusion.Resources.Domain.Queries;
+using MediatR;
+
+namespace Fusion.Resources.Api.FusionEvents.Handlers.InternalRequests
+{
+    public class UpdateOrgPositionInstanceHaveRequestHandler : INotificationHandler<InternalRequestCreated>, INotificationHandler<InternalRequestDeleted>
+    {
+        private readonly IMediator mediator;
+
+        public UpdateOrgPositionInstanceHaveRequestHandler(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        public async Task Handle(InternalRequestCreated notification, CancellationToken cancellationToken)
+        {
+            var request = await mediator.Send(new GetResourceAllocationRequestItem(notification.RequestId));
+            if (request is null)
+                return;
+
+            await mediator.Send(new Logic.Commands.UpdateOrgPositionInstanceHaveRequest(request.Project.OrgProjectId, request.OrgPosition!.Id, request.OrgPositionInstance!.Id, true));
+        }
+
+        public async Task Handle(InternalRequestDeleted notification, CancellationToken cancellationToken)
+        {
+            await mediator.Send(new Logic.Commands.UpdateOrgPositionInstanceHaveRequest(notification.OrgProjectId, notification.OrgPositionId!.Value, notification.PositionInstanceId!.Value, false));
+        }
+    }
+}
---
+++ src/backend/api/Fusion.Resources.Logic/Requests/Commands/ResourceAllocationRequest/UpdateOrgPositionInstanceHaveRequest.cs
@@ -0,0 +1,71 @@
+﻿using Fusion.ApiClients.Org;
+using MediatR;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fusion.Resources.Logic.Commands
+{
+    public class UpdateOrgPositionInstanceHaveRequest : IRequest
+    {
+        public UpdateOrgPositionInstanceHaveRequest(Guid orgProjectId, Guid orgPositionId, Guid orgPositionInstanceId, bool haveRequest)
+        {
+            OrgProjectId = orgProjectId;
+            OrgPositionId = orgPositionId;
+            OrgPositionInstanceId = orgPositionInstanceId;
+            HaveRequest = haveRequest;
+        }
+
+        public Guid OrgProjectId { get; }
+        public Guid OrgPositionId { get; }
+        public Guid OrgPositionInstanceId { get; }
+        public bool HaveRequest { get; }
+
+        public class Handler : AsyncRequestHandler<UpdateOrgPositionInstanceHaveRequest>
+        {
+            private readonly IOrgApiClient client;
+
+            public Handler(IOrgApiClientFactory orgApiClientFactory)
+            {
+                this.client = orgApiClientFactory.CreateClient(ApiClientMode.Application);
+            }
+
+            protected override async Task Handle(UpdateOrgPositionInstanceHaveRequest request, CancellationToken cancellationToken)
+            {
+
+
+                var position = await client.GetPositionV2Async(request.OrgProjectId, request.OrgPositionId);
+
+                var instance = position?.Instances.FirstOrDefault(i => i.Id == request.OrgPositionInstanceId);
+                if (instance is null)
+                    throw new InvalidOperationException($"Could not locate instance {request.OrgPositionInstanceId} on the position {request.OrgPositionId} for project {request.OrgProjectId}.");
+
+                var instancePatchRequest = new JObject();
+                instance.Properties = EnsureHasRequestProperty(instance.Properties, request.HaveRequest);
+                instancePatchRequest.SetPropertyValue<ApiPositionInstanceV2>(i => i.Properties, instance.Properties);
+
+                var url = $"/projects/{request.OrgProjectId}/positions/{request.OrgPositionId}/instances/{request.OrgPositionInstanceId}?api-version=2.0";
+                var updateResp = await client.PatchAsync<ApiPositionInstanceV2>(url, instancePatchRequest);
+
+                if (!updateResp.IsSuccessStatusCode)
+                    throw new OrgApiError(updateResp.Response, updateResp.Content);
+            }
+
+            private static ApiPropertiesCollectionV2 EnsureHasRequestProperty(ApiPropertiesCollectionV2? properties, bool value)
+            {
+                properties ??= new ApiPropertiesCollectionV2();
+                if (properties.ContainsKey("hasRequest", true))
+                {
+                    properties["hasRequest"] = value;
+                }
+                else
+                {
+                    properties.Add("hasRequest", value);
+                }
+                return properties;
+            }
+        }
+    }
+}
