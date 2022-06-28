9afba91f3943a1a1d1e46dc4822c5d32f23506e5
Lorentz Vedeler <lrvedeler@gmail.com>
2022-06-28 15:52:58 +0200

    update fusion libraries

--- src/backend/api/Fusion.Resources.Domain/Extensions/LineOrgExtensions.cs
+++ src/backend/api/Fusion.Resources.Domain/Extensions/LineOrgExtensions.cs
@@ -1,7 +1,8 @@
-﻿using Fusion.Integration;
+using Fusion.Integration;
 using Fusion.Integration.LineOrg;
 using Fusion.Integration.Profile;
 using Fusion.Resources.Domain;
+using Fusion.Services.LineOrg.ApiModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,29 +11,29 @@ namespace Fusion.Resources.Application
 {
     public static class LineOrgExtensions
     {
-        public static async Task<List<LineOrgDepartment>?> ResolveDepartmentChildrenAsync(this ILineOrgResolver resolver, string departmentId)
+        public static async Task<List<ApiDepartment>?> ResolveDepartmentChildrenAsync(this ILineOrgResolver resolver, string departmentId)
         {
-            var department = await resolver.ResolveDepartmentAsync(departmentId);
+            var department = await resolver.ResolveDepartmentAsync(DepartmentId.FromFullPath(departmentId));
             if (department is null) return null;
 
             return await resolver.ResolveDepartmentChildrenAsync(department);
         }
 
-        public static async Task<List<LineOrgDepartment>?> ResolveDepartmentChildrenAsync(this ILineOrgResolver resolver, LineOrgDepartment department)
+        public static async Task<List<ApiDepartment>?> ResolveDepartmentChildrenAsync(this ILineOrgResolver resolver, ApiDepartment department)
         {
             if (department?.Children is null) return null;
 
-            var children = new List<LineOrgDepartment>();
+            var children = new List<ApiDepartment>();
             foreach (var child in department.Children)
             {
-                var childDepartment = await resolver.ResolveDepartmentAsync(child.FullName);
+                var childDepartment = await resolver.ResolveDepartmentAsync(DepartmentId.FromFullPath(child.FullName));
                 if (childDepartment is not null) children.Add(childDepartment);
             }
 
             return children;
         }
 
-        public static async Task<List<QueryDepartment>> ToQueryDepartment(this IEnumerable<LineOrgDepartment> departments, IFusionProfileResolver profileResolver)
+        public static async Task<List<QueryDepartment>> ToQueryDepartment(this IEnumerable<ApiDepartment> departments, IFusionProfileResolver profileResolver)
         {
             var managerIds = departments
                     .Where(x => x.Manager?.AzureUniqueId != null)
@@ -49,7 +50,7 @@ namespace Fusion.Resources.Application
             return departments.Select(x => new QueryDepartment(x, x.Manager != null ? profileLookup[x.Manager!.AzureUniqueId!] : null)).ToList();
         }
 
-        public static async Task<List<QueryDepartment>> ToQueryDepartment(this IEnumerable<LineOrgUser> users, IFusionProfileResolver profileResolver)
+        public static async Task<List<QueryDepartment>> ToQueryDepartment(this IEnumerable<ApiLineOrgUser> users, IFusionProfileResolver profileResolver)
         {
             var managerIds = users.Select(x => new PersonIdentifier(x.AzureUniqueId));
 
@@ -62,7 +63,7 @@ namespace Fusion.Resources.Application
             }
 
             var profileLookup = profiles.ToDictionary(x => x.Profile!.AzureUniqueId!.Value, x => x.Profile!);
-            return users.Select(x => new QueryDepartment(new LineOrgDepartment
+            return users.Select(x => new QueryDepartment(new ApiDepartment
             {
                 Name = x.Department!,
                 FullName = x.FullDepartment!,
