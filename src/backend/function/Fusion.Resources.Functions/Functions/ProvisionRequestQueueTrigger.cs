4ef4551e917016a4d347863df3392ae2cebbcd57
terjebra <terje.bradland@gmail.com>
2022-09-17 20:07:35 +0200

    Removed function

--- src/backend/function/Fusion.Resources.Functions/Functions/ProvisionRequestQueueTrigger.cs
+++ src/backend/function/Fusion.Resources.Functions/Functions/ProvisionRequestQueueTrigger.cs
@@ -4,8 +4,6 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Fusion.Resources.Integration.Models.Queue;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.ServiceBus;
 using Microsoft.Extensions.Logging;
@@ -39,35 +37,11 @@ namespace Fusion.Resources.Functions
 
             var wasProvisioned = payload.Type switch
             {
-                ProvisionPositionMessageV1.RequestTypeV1.ContractorPersonnel => await ProvisionContractorPersonnel(log, payload),
                 ProvisionPositionMessageV1.RequestTypeV1.InternalPersonnel => await ProvisionInternalPersonnel(log, payload),
                 _ => throw new InvalidOperationException($" {payload.Type} is not a supported provisioning type.")
             };
 
             log.LogTrace($"Position was provisioned?: {wasProvisioned}");
-        }
-
-        private async Task<bool> ProvisionContractorPersonnel(ILogger log, ProvisionPositionMessageV1 payload)
-        {
-            var provisionResponse = await resourcesClient.PostAsync(
-                $"/projects/{payload.ProjectOrgId}/contracts/{payload.ContractOrgId}/resources/requests/{payload.RequestId}/provision",
-                null);
-
-            var content = await provisionResponse.Content.ReadAsStringAsync();
-
-            if (!provisionResponse.IsSuccessStatusCode)
-            {
-                log.LogError($"An error occured when trying to provision the request with id {payload.RequestId}");
-                log.LogError(content);
-
-                throw new Exception($"An error occured when trying to provision the request with id {payload.RequestId}");
-            }
-            else
-            {
-                log.LogInformation($"Successfully provisioned the position for request {payload.RequestId}");
-                log.LogInformation(content);
-            }
-            return true;
         }
         
         private async Task<bool> ProvisionInternalPersonnel(ILogger log, ProvisionPositionMessageV1 payload)
