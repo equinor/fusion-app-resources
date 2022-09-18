dc1827f3bb6456e5af5a90c8bb5ef7fb83d3436e
terjebra <terje.bradland@gmail.com>
2022-09-18 19:10:32 +0200

    Mark as oboslete

--- src/backend/api/Fusion.Resources.Api/Controllers/Mpp/MppController.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Mpp/MppController.cs
@@ -9,12 +9,14 @@ namespace Fusion.Resources.Api.Controllers.Mpp
     [ApiController]
     public class MppController : ResourceControllerBase
     {
+        [Obsolete]
         [HttpDelete("/projects/{projectIdentifier}/contracts/{contractIdentifier}/mpp/positions/{positionId}")]
         public ActionResult DeleteContractPosition([FromRoute] PathProjectIdentifier projectIdentifier, Guid contractIdentifier, Guid positionId)
         {
             return StatusCode(StatusCodes.Status410Gone);
         }
 
+        [Obsolete]
         [HttpOptions("/projects/{projectIdentifier}/contracts/{contractIdentifier}/mpp/positions")]
         public ActionResult CheckDeleteAccess([FromRoute] PathProjectIdentifier projectIdentifier, Guid contractIdentifier)
         {
