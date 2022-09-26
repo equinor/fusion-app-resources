d082243414a8b00cf2630e467ddf4687914f4deb
Svein Inge Carlsen <epostkasse25@hotmail.com>
2022-09-26 12:05:37 +0200

    Try to remove cached profile whenever a profile updated event is received. (#527)

--- src/backend/api/Fusion.Resources.Domain/NotificationHandlers/ProfileUpdatedHandler.cs
+++ src/backend/api/Fusion.Resources.Domain/NotificationHandlers/ProfileUpdatedHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Fusion.Integration.Diagnostics;
 using Fusion.Integration.Profile;
+using Fusion.Integration.Profile.Internal;
 using Fusion.Resources.Database;
 using Fusion.Resources.Database.Entities;
 using MediatR;
@@ -16,11 +17,13 @@ namespace Fusion.Resources.Domain.Notifications
         {
             private readonly ResourcesDbContext dbContext;
             private readonly IFusionLogger<ProfileUpdatedHandler> logger;
+            private readonly IProfileCache fusionProfileResolverCache;
 
-            public ProfileUpdatedHandler(ResourcesDbContext dbContext, IFusionLogger<ProfileUpdatedHandler> logger)
+            public ProfileUpdatedHandler(ResourcesDbContext dbContext, IFusionLogger<ProfileUpdatedHandler> logger, IProfileCache fusionProfileResolverCache)
             {
                 this.dbContext = dbContext;
                 this.logger = logger;
+                this.fusionProfileResolverCache = fusionProfileResolverCache;
             }
 
             public async Task Handle(ProfileUpdated notification, CancellationToken cancellationToken)
@@ -48,7 +51,11 @@ namespace Fusion.Resources.Domain.Notifications
 
                     await dbContext.SaveChangesAsync();
                     logger.LogInformation("Updated profile for {UPN} ({UniqueId})", notification.Profile.UPN, notification.Profile.AzureUniqueId);
+
                 }
+
+                // Received profile updated event. Remove from profile resolver cache to get a fresh profile in next request.
+                await fusionProfileResolverCache.RemoveAsync(notification.Profile.Identifier);
             }
 
             private async Task<DbPerson?> ResolveDbPersonAsync(FusionPersonProfile profile)
