cb1b8576ef9a32c60dab3baaab214253f927e6bf
oivheg <oivheg@gmail.com>
2022-10-10 09:25:42 +0200

    #Crated dotnet ef migration for DelegatedDepartmentResposibility context that was deleted (#531)

---
+++ src/backend/api/Fusion.Resources.Database/Migrations/20221006104956_DeleteDelegatedDepartmentResponsibility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fusion.Resources.Database.Migrations
+{
+    public partial class DeleteDelegatedDepartmentResponsibility : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentResponsibles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DepartmentResponsibles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false, defaultValueSql: "getutcdate()"),
+                    DateFrom = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateTo = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true, defaultValueSql: "getutcdate()"),
+                    DepartmentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ResponsibleAzureObjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentResponsibles", x => x.Id);
+                });
+        }
+    }
+}
