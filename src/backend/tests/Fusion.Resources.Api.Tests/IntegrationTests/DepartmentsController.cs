ea3ab4d182755925f1f4030c60e4ad730b993fac
Øivind Heggland <oivheg@gmail.com>
2022-10-19 13:18:55 +0200

    changed test code to check if it works now

--- src/backend/tests/Fusion.Resources.Api.Tests/IntegrationTests/DepartmentsController.cs
+++ src/backend/tests/Fusion.Resources.Api.Tests/IntegrationTests/DepartmentsController.cs
@@ -113,7 +113,8 @@ namespace Fusion.Resources.Api.Tests.IntegrationTests
             var resp = await Client.TestClientGetAsync<List<TestDepartment>>($"/departments?$search={mainResourceOwner.Name}");
 
             resp.Response.StatusCode.Should().Be(HttpStatusCode.OK);
-            resp.Value.Should().Contain(x => x.Name == delegatedDepartment && x.DelegatedResponsibles.First().AzureUniquePersonId.Equals(delegatedResourceOwner.AzureUniqueId));
+            //resp.Value.Should().Contain(x => x.Name == delegatedDepartment && x.DelegatedResponsibles.First().AzureUniquePersonId.Equals(delegatedResourceOwner.AzureUniqueId));
+            resp.Value.Should().Contain(x => x.Name == delegatedDepartment && x.DelegatedResponsibles.Any(y => y.AzureUniquePersonId.Equals(delegatedResourceOwner.AzureUniqueId)));
         }
 
         [Fact]
