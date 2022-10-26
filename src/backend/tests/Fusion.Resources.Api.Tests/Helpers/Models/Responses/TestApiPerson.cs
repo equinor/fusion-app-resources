c117ee8306d7f78a70e25fbab741923fdb858def
Øivind Heggland <oivheg@gmail.com>
2022-10-26 12:22:24 +0200

    #forogt to remove the unnesseasry GUID

--- src/backend/tests/Fusion.Resources.Api.Tests/Helpers/Models/Responses/TestApiPerson.cs
+++ src/backend/tests/Fusion.Resources.Api.Tests/Helpers/Models/Responses/TestApiPerson.cs
@@ -7,7 +7,6 @@ namespace Fusion.Testing.Mocks
     public class TestApiPerson
     {
         public Guid? AzureUniquePersonId { get; set; }
-        public Guid? AzureUniqueId { get; set; }
         public string Name { get; set; } = null!;
         public string Mail { get; set; } = null!;
         public string? FullDepartment { get; set; }
