f7808b1a0283a97e15d478ae46d81cf69ab9710a
Svein Inge Carlsen <epostkasse25@hotmail.com>
2023-01-12 19:36:17 +0100

    input validation

--- src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/AddDelegatedResourceOwnerRequest.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/AddDelegatedResourceOwnerRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 
 namespace Fusion.Resources.Api.Controllers
 {
@@ -8,5 +9,16 @@ namespace Fusion.Resources.Api.Controllers
         public DateTimeOffset DateTo { get; set; }
         public DateTimeOffset DateFrom { get; set; }
         public string? Reason { get; set; }
+
+        public class Validator : AbstractValidator<AddDelegatedResourceOwnerRequest>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.ResponsibleAzureUniqueId).NotEmpty();
+                RuleFor(x => x.DateFrom).GreaterThanOrEqualTo(DateTimeOffset.UtcNow.AddDays(-1));
+                RuleFor(x => x.DateTo).GreaterThanOrEqualTo(DateTimeOffset.UtcNow.AddYears(3));
+                RuleFor(x => x.Reason).NotContainScriptTag().MaximumLength(500);
+            }
+        }
     }
 }
