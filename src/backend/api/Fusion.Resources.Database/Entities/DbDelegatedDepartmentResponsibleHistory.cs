5d6d79caeae7b6137ed0a7fced7f81a6e57be76f
Svein Inge Carlsen <epostkasse25@hotmail.com>
2023-02-14 10:36:42 +0100

    New hosted service running periodically and moving expired delegated department responsible data to history table. Test also included (#561)

---
+++ src/backend/api/Fusion.Resources.Database/Entities/DbDelegatedDepartmentResponsibleHistory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fusion.Resources.Database.Entities;
+
+public class DbDelegatedDepartmentResponsibleHistory
+{
+    public Guid Id { get; set; }
+    [MaxLength(200)]
+    public string DepartmentId { get; set; } = null!;
+    public Guid ResponsibleAzureObjectId { get; set; }
+    public DateTimeOffset DateFrom { get; set; }
+    public DateTimeOffset DateTo { get; set; }
+    public DateTimeOffset DateCreated { get; set; }
+    public DateTimeOffset? DateUpdated { get; set; }
+    public DateTimeOffset Archived { get; set; }
+    public Guid? UpdatedBy { get; set; }
+    public string? Reason { get; set; }
+
+
+    internal static void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbDelegatedDepartmentResponsibleHistory>()
+            .HasKey(r => r.Id);
+    }
+}
