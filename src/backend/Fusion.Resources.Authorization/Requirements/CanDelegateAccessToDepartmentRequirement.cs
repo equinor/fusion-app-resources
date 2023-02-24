45b68525a11411c30eb252213e07ff148f397b51
Svein Inge Carlsen <epostkasse25@hotmail.com>
2023-02-24 15:39:09 +0100

    typo fixed

--- src/backend/Fusion.Resources.Authorization/Requirements/CanDelegateAccessToDepartmentRequirement.cs
+++ src/backend/Fusion.Resources.Authorization/Requirements/CanDelegateAccessToDepartmentRequirement.cs
@@ -10,7 +10,7 @@ namespace Fusion.Resources.Api.Authorization.Requirements
             this.Department = department;
         }
 
-        public override string Description => "The user must be resourceowner in this or any descentant department.";
+        public override string Description => "The user must be resourceowner in this or any descendant department.";
         public override string Code => "CanDelegateAccess";
 
         public string Department { get; }
