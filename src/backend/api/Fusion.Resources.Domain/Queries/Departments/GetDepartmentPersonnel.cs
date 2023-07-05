46412fe438b2f63a3bd52b8f1c95d93fb6a0fc10
Øivind Heggland <oivheg@gmail.com>
2023-07-05 09:54:22 +0200

    #minor test

--- src/backend/api/Fusion.Resources.Domain/Queries/Departments/GetDepartmentPersonnel.cs
+++ src/backend/api/Fusion.Resources.Domain/Queries/Departments/GetDepartmentPersonnel.cs
@@ -211,7 +211,7 @@ namespace Fusion.Resources.Domain
                 var removeManagerQuery = string.Join(" and ", managers.Select(m => $"azureUniqueId ne '{m}'"));
                 var queryString = (managers.Any() ? removeManagerQuery + " and " : "") + $"fullDepartment eq '{fullDepartmentString}' and isExpired eq false";
 
-                // This Code appear to be no longer needed as we do not need to find based on manager, this also creates problems when manager is manager in mulitple departments
+                // This Code appears to be no longer needed as we do not need to find based on manager, this also creates problems when manager is manager in mulitple departments
                 //if (managers.Any())
                 //    queryString += " or " + string.Join(" or ", managers.Select(m => $"managerAzureId eq '{m}'"));
 
