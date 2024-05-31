fc6942b6c6603f5e981e3b148d41c3a36170e341
Hans Dahle <HANDAH@equinor.com>
2024-05-31 07:25:59 +0200

    Added fields to api model for richer response as we now have more data on org units

--- src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/ApiDepartment.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/ApiDepartment.cs
@@ -7,14 +7,30 @@ namespace Fusion.Resources.Api.Controllers
     public class ApiDepartment
     {
         public ApiDepartment(QueryDepartment department)
-        {            
+        {
+            // Keeping the name property for backwards compatebility. Should be refactored in new endpoint or version.
             Name = department.FullDepartment;
+            SapId = department.Identifier;
+            Title = department.Name;
+
+            ShortName = department.ShortName;
+            FullDepartment = department.FullDepartment;
             Sector = department.SectorId;
             LineOrgResponsible = (department.LineOrgResponsible is not null) ? new ApiPerson(department.LineOrgResponsible) : null;
             DelegatedResponsibles = (department.DelegatedResourceOwners is not null) 
                 ? department.DelegatedResourceOwners.Select(x => new ApiPerson(x)).ToList() 
                 : null;
         }
+
+        /// <summary>
+        /// The identifier for the org unit. Will be SAP id, but could potentially be workday in the future.
+        /// Using sapid as name for consistancy across. Will hopefully be maintained in workday as well.
+        /// </summary>
+        public string? SapId { get; set; }
+        public string? Title { get; set; }
+        public string? ShortName { get; set; }
+
+        public string FullDepartment { get; set; }
 
         public string Name { get; set; }
         public string? Sector { get; set; }
