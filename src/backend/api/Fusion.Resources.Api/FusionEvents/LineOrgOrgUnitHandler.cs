47216b51e892b65bf8a342f386ac8d571cef299b
Hans Dahle <handah@equinor.com>
2024-06-04 13:17:09 +0200

    feat: Update assigned department on request when org unit is renamed (#639)
    - [x] New feature
    - [ ] Bug fix
    - [ ] High impact
    
    **Description of work:**
    When org units are renamed or moved we still persist the old full
    department string. This can leave requests connected to org units that
    no longer exists -> orphan requests.
    
    We can subscribe for events comming from line org to act when these
    changes are done. This should be done as a persistant event handler so
    we are guaranteed delivery in case of downtime etc.
    
    - Added persistant event handler to perform sync on assigned department
    for requests
    
    The transient handler are used to invalidate cache, as this is executed
    on all instances of the api.
    
    
    **Testing:**
    - [x] Can be tested
    - [ ] ~~Automatic tests created / updated~~
    - [x] Local tests are passing
    
    Can be verified by assigning a ticket to some department (use one not
    commonly used, so we dont cause much problems).
    PATCH the org unit in lineorg, chaning the full department (add a
    character etc).
    Verify requests are assigned to that department
    PATCH the org unit back to the original value (clean up)
    Can verify requests are assigned back
    
    
    **Checklist:**
    - [ ] ~~Considered automated tests~~
    - [ ] ~~Considered updating specification / documentation~~
    - [x] Considered work items 
    - [x] Considered security
    - [x] Performed developer testing
    - [x] Checklist finalized / ready for review
    
    
    [AB#53786](https://statoil-proview.visualstudio.com/787035c2-8cf2-4d73-a83e-bb0e6d937eec/_workitems/edit/53786)
    Limited changes to only requests. There are other entities which stores
    full department reference. These should be changed in own prs.

--- src/backend/api/Fusion.Resources.Api/FusionEvents/LineOrgOrgUnitHandler.cs
+++ src/backend/api/Fusion.Resources.Api/FusionEvents/LineOrgOrgUnitHandler.cs
@@ -5,11 +5,18 @@ using Fusion.Resources.Domain;
 using Microsoft.Extensions.Caching.Memory;
 using Fusion.Resources.Application.LineOrg;
 using Fusion.Resources.Api.Controllers;
+using Newtonsoft.Json;
 
 namespace Fusion.Resources.Api
 {
+
     /// <summary>
     /// Handler listening on Line-Org service changes to org-units.
+    /// 
+    /// Transient handler which should be executed in all instances of the api. 
+    /// Primarily used for invalidating and clearing cache. 
+    /// 
+    /// Operations that modify data should be run in the persistant handler.
     /// </summary>
     public class LineOrgOrgUnitHandler : ISubscriptionHandler
     {
@@ -27,8 +34,40 @@ namespace Fusion.Resources.Api
             await orgUnitCache.ClearOrgUnitCacheAsync();
             cache.Remove(LineOrgClient.OrgUnitsMemCacheKey);
 
-            // Just clearing all org unit cache if there are changes.
-            OrgUnitResolver.ClearCache();
+            // Process the even 
+            switch (ctx.Event.Type)
+            {
+                case var org when org == LineOrgEventTypes.OrgUnit.Name:
+                    await HandleOrgUnitEventAsync(body);
+                    break;
+            }
         }
+
+        private Task HandleOrgUnitEventAsync(string? body)
+        {   
+            var payloadData = JsonConvert.DeserializeObject<LineOrgEventBody>(body ?? "");
+
+            if (payloadData is null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (payloadData.GetChangeType() == LineOrgEventBody.ChangeType.Updated)
+            {
+                OrgUnitResolver.ClearCache(payloadData.SapId);
+                OrgUnitResolver.ClearCache(payloadData.FullDepartment);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /* Example payload
+        * {
+        *  "SapId":"53079987",                      // Sap id, should not change
+        *  "FullDepartment":"EPI DEV CADS BRA",     // The fullDepartment, pre update
+        *  "Type":"Updated",
+        *  "Changes":["Name","ShortName","Department","FullDepartment","ParentSapId","Parents"]
+        * }
+        */
     }
 }
---
+++ src/backend/api/Fusion.Resources.Api/FusionEvents/LineOrgSyncronizationHandler.cs
@@ -0,0 +1,81 @@
+﻿using Fusion.Events;
+using System.Threading;
+using System.Threading.Tasks;
+using Fusion.Resources.Domain;
+using Newtonsoft.Json;
+using MediatR;
+using System;
+using System.Linq;
+
+namespace Fusion.Resources.Api
+{
+    /// <summary>
+    /// Handler to execute syncronisation events from the line org service. 
+    /// We only want updates to be executed by a single instance in case of multiple instances running the api. 
+    /// 
+    /// The handler should accept events, collect relevant data, determine if event is relevant and dispatch events the domain should handle to perform updates.
+    /// 
+    /// Should consider what the handler actually does (running in context of the event execution) or if some work could be dispatched to an async handler (e.g. service bus). 
+    /// If the handler throws an unhandled exception, the whole processing will be terminated and re-executed as the message is retried. If it consistently fail it will fall to a dead letter queue.
+    /// 
+    /// </summary>
+    public class LineOrgSyncronizationHandler : ISubscriptionHandler
+    {
+        private readonly IMediator mediator;
+
+        public LineOrgSyncronizationHandler(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public async Task ProcessMessageAsync(MessageContext ctx, string? body, CancellationToken cancellationToken)
+        {
+
+            // Process the even 
+            switch (ctx.Event.Type)
+            {
+                case var org when org == LineOrgEventTypes.OrgUnit.Name:
+                    await HandleOrgUnitEventAsync(body);
+                    break;
+            }
+        }
+
+        private async Task HandleOrgUnitEventAsync(string? body)
+        {
+            var payloadData = JsonConvert.DeserializeObject<LineOrgEventBody>(body ?? "");
+
+            if (payloadData is null)
+            {
+                return;
+            }
+
+            if (payloadData.GetChangeType() == LineOrgEventBody.ChangeType.Updated)
+            {
+                if (payloadData.Changes?.Any(i => i.EqualsIgnCase("fullDepartment")) == true) 
+                {
+                    await HandleOrgUnitUpdatedAsync(payloadData);
+                }
+            }
+        }
+
+        private async Task HandleOrgUnitUpdatedAsync(LineOrgEventBody payloadData)
+        {            
+            // Need to disable cache 
+            using var disableCacheScope = new Fusion.Integration.DisableCacheScope();   // Need to verify this works..
+
+            var orgUnit = await mediator.Send(new ResolveLineOrgUnit(payloadData.SapId));
+
+            if (orgUnit is null) 
+            {
+                throw new InvalidOperationException("Updated org unit could no longer be resolved. Was it deleted as well?");
+            }
+
+            if (orgUnit.FullDepartment.EqualsIgnCase(payloadData.FullDepartment))
+            {
+                throw new InvalidOperationException("Resolved fullDepartment is same as existing. Caching issue? Terminating operation and hoping retry will solve issue.");
+            }
+
+            await mediator.Publish(new Domain.Notifications.System.OrgUnitPathUpdated(payloadData.SapId, payloadData.FullDepartment, orgUnit.FullDepartment));
+        }
+    }
+}
