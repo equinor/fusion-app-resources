14af6ee6a7a94a43eb0cc8a05de019d36b8f4107
Lars Fagerbakke <larf@equinor.com>
2024-06-10 12:00:13 +0200

    Added tests to IDepartmentService implementation

---
+++ src/tests/Fusion.Summary.Api.Tests/UnitTest1.cs
@@ -0,0 +1,136 @@
+using Fusion.Summary.Api.Database;
+using Fusion.Summary.Api.Database.Entities;
+using Fusion.Summary.Api.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Fusion.Summary.Api.Tests;
+
+[TestClass]
+public class DepartmentServiceTests
+{
+    private DatabaseContext _context;
+    private IDepartmentService _departmentService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Use InMemory database instead
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase(databaseName: "test_db").Options;
+
+        // Create a new instance of the DatabaseContext
+        _context = new DatabaseContext(options);
+        _departmentService = new DepartmentService(_context);
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        // Cleanup the in-memory database after each test run
+        _context.Database.EnsureDeleted();
+    }
+
+    [TestMethod]
+    public async Task CreateDepartment_ShouldReturnTrue()
+    {
+        // Arrange
+        var department = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A" };
+
+        // Act
+        var result = await _departmentService.CreateDepartment(department);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public async Task GetAllDepartments_ShouldReturnAllDepartments()
+    {
+        // Arrange
+        var departmentA = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A" };
+        var departmentB = new DepartmentTableEntity { DepartmentSapId = "1002", FullDepartmentName = "Department B" };
+        await _departmentService.CreateDepartment(departmentA);
+        await _departmentService.CreateDepartment(departmentB);
+
+        // Act
+        var result = await _departmentService.GetAllDepartments();
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(2, result.Count);
+        Assert.IsTrue(result.Contains(departmentA));
+        Assert.IsTrue(result.Contains(departmentB));
+    }
+
+    [TestMethod]
+    public async Task GetDepartmentById_ExistingId_ShouldReturnDepartment()
+    {
+        // Arrange
+        var departmentA = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A" };
+        var departmentB = new DepartmentTableEntity { DepartmentSapId = "1002", FullDepartmentName = "Department B" };
+        await _departmentService.CreateDepartment(departmentA);
+        await _departmentService.CreateDepartment(departmentB);
+
+        // Act
+        var result = await _departmentService.GetDepartmentById("1001");
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(departmentA, result);
+    }
+
+    [TestMethod]
+    public async Task GetDepartmentById_NonExistingId_ShouldReturnNull()
+    {
+        // Arrange
+
+        // Act
+        var result = await _departmentService.GetDepartmentById("1001");
+
+        // Assert
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public async Task UpdateDepartment_ExistingId_ShouldUpdateDepartment()
+    {
+        // Arrange
+        var resourceOwner1 = Guid.Parse("00000000-0000-0000-0000-000000000001");
+        var resourceOwner2 = Guid.Parse("00000000-0000-0000-0000-000000000002");
+
+        var departmentA = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A", ResourceOwnerAzureUniqueId = resourceOwner1 };
+        var departmentB = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A", ResourceOwnerAzureUniqueId = resourceOwner2 };
+        
+        await _departmentService.CreateDepartment(departmentA);
+
+        // Act
+        await _departmentService.UpdateDepartment("1001", departmentB);
+        var result = await _departmentService.GetDepartmentById("1001");
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(resourceOwner2, result.ResourceOwnerAzureUniqueId);
+    }
+
+    [TestMethod]
+    public async Task UpdateDepartment_NonExistingId_ShouldNotUpdateDepartment()
+    {
+        // Arrange
+        var resourceOwner1 = Guid.Parse("00000000-0000-0000-0000-000000000001");
+        var resourceOwner2 = Guid.Parse("00000000-0000-0000-0000-000000000002");
+
+        var departmentA = new DepartmentTableEntity { DepartmentSapId = "1001", FullDepartmentName = "Department A", ResourceOwnerAzureUniqueId = resourceOwner1 };
+        var departmentB = new DepartmentTableEntity { DepartmentSapId = "1002", FullDepartmentName = "Department B", ResourceOwnerAzureUniqueId = resourceOwner2 };
+        
+        await _departmentService.CreateDepartment(departmentA);
+
+        // Act
+        await _departmentService.UpdateDepartment("1002", departmentB);
+        var result = await _departmentService.GetDepartmentById("1001");
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(resourceOwner1, result.ResourceOwnerAzureUniqueId);
+    }
+}
