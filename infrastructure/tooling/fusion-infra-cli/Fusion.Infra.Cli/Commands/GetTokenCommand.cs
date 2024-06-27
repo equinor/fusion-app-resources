65ed3dbf0d2f7dcc81fde0a269dfc61505b3ac92
Hans Dahle <HANDAH@equinor.com>
2024-06-27 12:45:01 +0200

    added token command

---
+++ infrastructure/tooling/fusion-infra-cli/Fusion.Infra.Cli/Commands/GetTokenCommand.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using McMaster.Extensions.CommandLineUtils;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fusion.Infra.Cli.Commands
+{
+    [Command("get-token")]
+    public class GetTokenCommand : CommandBase
+    {
+
+        [Required]
+        [Option("-r <resource>", ShortName = "r", LongName = "resource", Description = "Get access token to a specific resource")]
+        public string Resource { get; set; } = null!;
+
+
+        public override async Task OnExecuteAsync(CommandLineApplication app)
+        {
+            var credentials = new DefaultAzureCredential();
+            var token = await credentials.GetTokenAsync(new TokenRequestContext(new[] { Resource }), CancellationToken.None);
+
+            Console.WriteLine(token.Token);
+        }
+
+    }
+}
