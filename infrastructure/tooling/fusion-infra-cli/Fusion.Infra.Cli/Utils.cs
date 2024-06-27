5692bc4433d3ce4a8b2897a2a530302151a24593
Hans Dahle <HANDAH@equinor.com>
2024-06-27 08:23:32 +0200

    added command

---
+++ infrastructure/tooling/fusion-infra-cli/Fusion.Infra.Cli/Utils.cs
@@ -0,0 +1,37 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+
+namespace Fusion.Infra.Cli;
+
+public class Utils
+{
+
+    public static void AnalyseToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenContent = tokenHandler.ReadJwtToken(token);
+
+        Console.WriteLine($"-- Token | object id: {tokenContent.Claims}");
+    }
+
+    public static void PrintToken(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenContent = tokenHandler.ReadJwtToken(token);
+
+        var serializedContent = JsonSerializer.Serialize(new
+        {
+            claims = tokenContent.Claims.Select(c => new { type = c.Type, value = c.Value }),
+            actor = tokenContent.Actor,
+            audiences = tokenContent.Audiences,
+            issuedAt = tokenContent.IssuedAt,
+            id = tokenContent.Id,
+            subject = tokenContent.Subject,
+            validFrom = tokenContent.ValidFrom,
+            valiedTo = tokenContent.ValidTo
+        }, new JsonSerializerOptions(JsonSerializerDefaults.Web) { WriteIndented = true });
+
+        Console.WriteLine(serializedContent);
+    }
+}
