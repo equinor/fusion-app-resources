32944f512fe6c8b000d915bc93cb41df4641cfce
Lars Fagerbakke <larf@equinor.com>
2024-07-02 10:04:21 +0200

    Cleaned up base controller

--- src/Fusion.Summary.Api/BaseController.cs
+++ src/Fusion.Summary.Api/BaseController.cs
@@ -5,22 +5,6 @@ namespace Fusion.Summary.Api;
 
 public class BaseController : ControllerBase
 {
-    private CommandDispatcher? dispatcher = null;
-
-    public CommandDispatcher Commands
-    {
-        get
-        {
-            if (dispatcher is null)
-            {
-                var mediator = HttpContext.RequestServices.GetRequiredService<IMediator>();
-                dispatcher = new CommandDispatcher(mediator);
-            }
-
-            return dispatcher;
-        }
-    }
-
     protected Task DispatchCommandAsync(IRequest command)
     {
         var mediator = HttpContext.RequestServices.GetRequiredService<IMediator>();
@@ -33,25 +17,5 @@ public class BaseController : ControllerBase
         var mediator = HttpContext.RequestServices.GetRequiredService<IMediator>();
 
         return mediator.Send(command, HttpContext.RequestAborted);
-    }
-}
-
-public class CommandDispatcher
-{
-    public readonly IMediator mediator;
-
-    public CommandDispatcher(IMediator mediator)
-    {
-        this.mediator = mediator;
-    }
-
-    public Task DispatchAsync(IRequest command)
-    {
-        return mediator.Send(command);
-    }
-
-    public Task<TResult> DispatchAsync<TResult>(IRequest<TResult> command)
-    {
-        return mediator.Send(command);
     }
 }
