573c2784a47285d41669c6d7ca21918828c15a99
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-07-29 10:10:55 +0200

    feat: Summary AZ Function code setup and DepartmentResourceOwnerSync (#645)
    - [x] New feature
    - [ ] Bug fix
    - [ ] High impact
    
    **Work description**:
    
    Created a class library that contains common logic for the two function
    apps. It's namespace is `Fusion.Resources.Functions.Common`, I feel like
    there is a better name for it, but cant think of one.
    
    Added DepartmentResourceOwnerSync timer function that retrieves
    DepartmentResourceOwners for LineOrg and sends them to the new Summary
    API. It does not put anything on the buss as of now, thinking of adding
    that in a separate PR.
    
    There's also the question of wheter the summary app should have it's own
    application registration. As of now it uses resources' registration. But
    this result in minor code changes for this PR.
    
    **The `department-resource-owner-sync` is disabled because the Summary
    API still isn't merged in / being worked on**
    
    Link to workitem: US
    [AB#51132](https://statoil-proview.visualstudio.com/787035c2-8cf2-4d73-a83e-bb0e6d937eec/_workitems/edit/51132),
    task
    [AB#54679](https://statoil-proview.visualstudio.com/787035c2-8cf2-4d73-a83e-bb0e6d937eec/_workitems/edit/54679)
    
    **Test description**:
    
    - [ ] Can be tested
    - [ ] Automatic tests created / updated
    - [x] Local tests are passing
    
    
    **Checklist**:
    
    - [x] Considered automated tests
    - [ ] Considered updating specification / documentation
    - [x] Considered work items
    - [x] Considered security
    - [x] Performed developer testing
    - [x] Checklist finalized / ready for review

---
+++ src/Fusion.Resources.Functions.Common/ApiClients/OrgApiClient.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using Fusion.Resources.Functions.Common.Integration.Http;
+
+namespace Fusion.Resources.Functions.Common.ApiClients;
+
+public class OrgClient : IOrgClient
+{
+    private readonly HttpClient orgClient;
+
+    public OrgClient(IHttpClientFactory httpClientFactory)
+    {
+        orgClient = httpClientFactory.CreateClient(HttpClientNames.Application.Org);
+        orgClient.Timeout = TimeSpan.FromMinutes(5);
+    }
+
+    public async Task<ApiChangeLog> GetChangeLog(string projectId, DateTime timestamp)
+    {
+        var url = $"/projects/{projectId}/change-log?$filter=timestamp gt '{timestamp.ToString("yyyy-MM-dd")}'&api-version=2.0";
+        var data =
+            await orgClient.GetAsJsonAsync<ApiChangeLog>(
+                url);
+
+        return data;
+    }
+}
---
+++ src/Fusion.Resources.Functions.Common/ApiClients/PeopleApiClient.cs
@@ -0,0 +1,22 @@
+﻿using Fusion.Resources.Functions.Common.Integration.Http;
+
+namespace Fusion.Resources.Functions.Common.ApiClients;
+
+public class PeopleApiClient : IPeopleApiClient
+{
+    private readonly HttpClient peopleClient;
+
+    public PeopleApiClient(IHttpClientFactory httpClientFactory)
+    {
+        peopleClient = httpClientFactory.CreateClient(HttpClientNames.Application.People);
+        peopleClient.Timeout = TimeSpan.FromMinutes(5);
+    }
+
+    public async Task<string> GetPersonFullDepartmentAsync(Guid? personAzureUniqueId)
+    {
+        var data = await peopleClient.GetAsJsonAsync<IResourcesApiClient.Person>(
+            $"persons/{personAzureUniqueId}?api-version=3.0");
+
+        return data.FullDepartment;
+    }
+}
