344333e704d7eef6ab0ff0498bad49b9eccb47ed
Hans Dahle <handah@equinor.com>
2024-08-14 08:54:39 +0200

    Update Program.cs

--- src/backend/api/Fusion.Resources.Api/Program.cs
+++ src/backend/api/Fusion.Resources.Api/Program.cs
@@ -8,7 +8,7 @@ namespace Fusion.Resources.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)  
         {
             CreateHostBuilder(args).Build().Run();
         }
