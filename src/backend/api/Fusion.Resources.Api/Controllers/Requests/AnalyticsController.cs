1e1a423981c3ce10973fb368f8844e09faecfbd0
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-09-02 14:30:42 +0200

    Add missing ApiVersion attribute

--- src/backend/api/Fusion.Resources.Api/Controllers/Requests/AnalyticsController.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Requests/AnalyticsController.cs
@@ -14,6 +14,7 @@ namespace Fusion.Resources.Api.Controllers
 {
     [ApiVersion("1.0-preview")]
     [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [Authorize]
     [ApiController]
     public class AnalyticsController : ResourceControllerBase
