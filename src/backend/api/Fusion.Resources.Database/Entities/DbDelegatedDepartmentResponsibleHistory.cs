5b2840a638ce6f05abe515923c04ba337e8d0486
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-09-06 09:03:29 +0200

    Added OrgUnitDeleted.ArchiveDelegatedResourceOwnersHandler.cs

--- src/backend/api/Fusion.Resources.Database/Entities/DbDelegatedDepartmentResponsibleHistory.cs
+++ src/backend/api/Fusion.Resources.Database/Entities/DbDelegatedDepartmentResponsibleHistory.cs
@@ -7,8 +7,7 @@ namespace Fusion.Resources.Database.Entities;
 public class DbDelegatedDepartmentResponsibleHistory
 {
     public Guid Id { get; set; }
-    [MaxLength(200)]
-    public string DepartmentId { get; set; } = null!;
+    [MaxLength(200)] public string DepartmentId { get; set; } = null!;
     public Guid ResponsibleAzureObjectId { get; set; }
     public DateTimeOffset DateFrom { get; set; }
     public DateTimeOffset DateTo { get; set; }
@@ -17,6 +16,24 @@ public class DbDelegatedDepartmentResponsibleHistory
     public DateTimeOffset Archived { get; set; }
     public Guid? UpdatedBy { get; set; }
     public string? Reason { get; set; }
+
+    public DbDelegatedDepartmentResponsibleHistory()
+    {
+    }
+
+    public DbDelegatedDepartmentResponsibleHistory(DbDelegatedDepartmentResponsible dbDelegatedDepartmentResponsible)
+    {
+        Id = Guid.NewGuid();
+        Archived = DateTimeOffset.UtcNow;
+        DepartmentId = dbDelegatedDepartmentResponsible.DepartmentId;
+        ResponsibleAzureObjectId = dbDelegatedDepartmentResponsible.ResponsibleAzureObjectId;
+        DateFrom = dbDelegatedDepartmentResponsible.DateFrom;
+        DateTo = dbDelegatedDepartmentResponsible.DateTo;
+        DateCreated = dbDelegatedDepartmentResponsible.DateCreated;
+        DateUpdated = dbDelegatedDepartmentResponsible.DateUpdated;
+        UpdatedBy = dbDelegatedDepartmentResponsible.UpdatedBy;
+        Reason = dbDelegatedDepartmentResponsible.Reason;
+    }
 
 
     internal static void OnModelCreating(ModelBuilder modelBuilder)
---
+++ src/backend/api/Fusion.Resources.Domain/Notifications/System/OrgUnitDeleted.ArchiveDelegatedResourceOwnersHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Fusion.Resources.Database;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Fusion.Integration.Profile;
+using Fusion.Integration.Roles;
+using Fusion.Resources.Database.Entities;
+
+namespace Fusion.Resources.Domain.Notifications.System;
+
+public partial class OrgUnitDeleted
+{
+    /// <summary>
+    ///     Archive all delegated resource owners for the deleted department and remove their roles.
+    /// </summary>
+    public class ArchiveDelegatedResourceOwnersHandler : INotificationHandler<OrgUnitDeleted>
+    {
+        private readonly ILogger<ArchiveDelegatedResourceOwnersHandler> logger;
+        private readonly ResourcesDbContext db;
+        private readonly IFusionRolesClient rolesClient;
+
+
+        public ArchiveDelegatedResourceOwnersHandler(ILogger<ArchiveDelegatedResourceOwnersHandler> logger, ResourcesDbContext db, IFusionRolesClient rolesClient)
+        {
+            this.logger = logger;
+            this.db = db;
+            this.rolesClient = rolesClient;
+        }
+
+        public async Task Handle(OrgUnitDeleted notification, CancellationToken cancellationToken)
+        {
+            var delegatedResourceOwners = await db.DelegatedDepartmentResponsibles
+                .Where(r => r.DepartmentId == notification.FullDepartment)
+                .ToListAsync(cancellationToken);
+
+            if (delegatedResourceOwners.Count == 0)
+                return;
+
+            logger.LogInformation("Archiving {Count} delegated resource owners for deleted department {FullDepartment}", delegatedResourceOwners.Count, notification.FullDepartment);
+
+
+            foreach (var resourceOwner in delegatedResourceOwners)
+            {
+                try
+                {
+                    await rolesClient.DeleteRolesAsync(
+                        new PersonIdentifier(resourceOwner.ResponsibleAzureObjectId),
+                        q => q.WhereRoleName(AccessRoles.ResourceOwner).WhereScopeValue(notification.FullDepartment)
+                    );
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, "Failed to delete role for delegated resource owner {AzureUniqueId} in department {FullDepartment}", resourceOwner.ResponsibleAzureObjectId, notification.FullDepartment);
+                    // TODO: Should we stop the execution here? Use transactions?
+                    // throw;
+                }
+            }
+
+
+            db.DelegatedDepartmentResponsibles.RemoveRange(delegatedResourceOwners);
+
+            var archivedDelegateResourceOwners = delegatedResourceOwners
+                .Select(res => new DbDelegatedDepartmentResponsibleHistory(res));
+
+            db.DelegatedDepartmentResponsiblesHistory.AddRange(archivedDelegateResourceOwners);
+
+            await db.SaveChangesAsync(CancellationToken.None);
+        }
+    }
+}
