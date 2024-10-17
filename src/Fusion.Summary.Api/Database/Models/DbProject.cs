74a68bfd51c57c447802c3348ce8c25463e2ee89
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-10-17 11:45:17 +0200

    Init db tables

---
+++ src/Fusion.Summary.Api/Database/Models/DbProject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fusion.Summary.Api.Database.Models;
+
+public class DbProject
+{
+    public required Guid Id { get; set; }
+
+    public required string Name { get; set; }
+    public required Guid OrgProjectExternalId { get; set; }
+
+
+    internal static void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbProject>(project =>
+        {
+            project.ToTable("Projects");
+            project.HasKey(p => p.Id);
+            project.HasIndex(p => p.OrgProjectExternalId).IsUnique();
+        });
+    }
+}
---
+++ src/Fusion.Summary.Api/Database/Models/DbWeeklyTaskOwnerReport.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fusion.Summary.Api.Database.Models;
+
+public class DbWeeklyTaskOwnerReport
+{
+    public required Guid Id { get; set; }
+    public required Guid ProjectId { get; set; }
+    public DbProject? Project { get; set; }
+
+    public required DateTime Period { get; set; }
+
+    internal static void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbWeeklyTaskOwnerReport>(report =>
+        {
+            report.ToTable("WeeklyTaskOwnerReports");
+            report.HasKey(r => r.Id);
+
+            report.HasOne(r => r.Project)
+                .WithMany()
+                .HasForeignKey(r => r.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+    }
+}
+
+public class DbAdminAccessExpiring
+{
+    public required Guid AzureUniqueId { get; set; }
+    public required string FullName { get; set; }
+    public required DateTime Expires { get; set; }
+}
+
+public class DbActionsAwaitingTaskOwner
+{
+    // TODO: Implement
+}
+
+public class DbPositionAllocationsEnding
+{
+    // TODO: Implement
+}
+
+public class DbTBNPositionsStartingSoon
+{
+    // TODO: Implement
+}
