9d6b73d383d6f16130aae7888e794149335da8de
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-10-31 15:44:25 +0100

    Init WeeklyTaskOwnerReportWorker.cs

---
+++ src/Fusion.Summary.Functions/Functions/TaskOwnerReports/WeeklyTaskOwnerReportWorker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Fusion.Resources.Functions.Common.ApiClients;
+using Fusion.Resources.Functions.Common.Extensions;
+using Fusion.Summary.Functions.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Extensions.Logging;
+
+namespace Fusion.Summary.Functions.Functions.TaskOwnerReports;
+
+public class WeeklyTaskOwnerReportWorker
+{
+    private readonly ISummaryApiClient summaryApiClient;
+    private readonly ILogger<WeeklyTaskOwnerReportWorker> logger;
+
+    public WeeklyTaskOwnerReportWorker(ISummaryApiClient summaryApiClient, ILogger<WeeklyTaskOwnerReportWorker> logger)
+    {
+        this.summaryApiClient = summaryApiClient;
+        this.logger = logger;
+    }
+
+    private const string FunctionName = "weekly-task-owner-report-worker";
+
+    [FunctionName(FunctionName)]
+    public async Task RunAsync(
+        [ServiceBusTrigger("%project_summary_weekly_queue%", Connection = "AzureWebJobsServiceBus")]
+        ServiceBusReceivedMessage message, ServiceBusMessageActions messageReceiver)
+    {
+        var dto = await JsonSerializer.DeserializeAsync<WeeklyTaskOwnerReportMessage>(message.Body.ToStream());
+
+        logger.LogInformation("{FunctionName} started with message: {MessageBody}", FunctionName, dto.ToJson());
+        try
+        {
+            await CreateAndStoreReportAsync(dto);
+            await messageReceiver.CompleteMessageAsync(message);
+            logger.LogInformation($"{FunctionName} completed successfully");
+        }
+        catch (Exception e) // Dead letter message
+        {
+            logger.LogError(e, $"{FunctionName} completed with error");
+            throw;
+        }
+    }
+
+    private async Task CreateAndStoreReportAsync(WeeklyTaskOwnerReportMessage message)
+    {
+    }
+}
---
+++ src/Fusion.Summary.Functions/Models/WeeklyTaskOwnerReportMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fusion.Summary.Functions.Models;
+
+/// Message sent between weekly task owner report sync and worker
+[DebuggerDisplay("ProjectName: {ProjectName} - ProjectAdmins count: {ProjectAdmins.Length}")]
+public class WeeklyTaskOwnerReportMessage
+{
+    public required Guid ProjectId { get; init; }
+    public required Guid OrgProjectExternalId { get; init; }
+
+    /// Mainly for debugging purposes
+    public required string ProjectName { get; init; }
+
+    public required ProjectAdmin[] ProjectAdmins { get; init; }
+
+    [DebuggerDisplay("AzureUniqueId: {AzureUniqueId} - Mail: {Mail} - ValidTo: {ValidTo}")]
+    public class ProjectAdmin
+    {
+        public required Guid AzureUniqueId { get; init; }
+
+        public string? Mail { get; init; }
+
+        /// Can be null due to being nullable from the roles api
+        public required DateTimeOffset? ValidTo { get; init; }
+    }
+}
