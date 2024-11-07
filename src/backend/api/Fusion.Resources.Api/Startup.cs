9deb9b0265ad1cb069668a9820183584d9066e21
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2024-11-07 12:49:18 +0100

    TEST: Trigger resources pr pipeline

--- src/backend/api/Fusion.Resources.Api/Startup.cs
+++ src/backend/api/Fusion.Resources.Api/Startup.cs
@@ -35,6 +35,7 @@ namespace Fusion.Resources.Api
         }
         
 
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
