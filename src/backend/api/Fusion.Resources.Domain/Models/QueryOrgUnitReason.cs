97d36502ec37dcd8791e23965f759e8a2c06b941
Hans Dahle <handah@equinor.com>
2024-11-24 19:09:59 +0100

    fix: Fixed profile and relevant-department to resolve correctly after workday update (#725)
    - [ ] New feature
    - [x] Bug fix
    - [ ] High impact
    
    **Description of work:**
    Get person departments was missed when aligning endpoints to use fusion
    roles for manager responebility instead of isResourceOwner and
    fullDepartment.
    
    
    **Testing:**
    - [x] Can be tested
    - [x] Automatic tests created / updated
    - [x] Local tests are passing
    
    
    
    **Checklist:**
    - [ ] Considered automated tests
    - [ ] Considered updating specification / documentation
    - [ ] Considered work items 
    - [ ] Considered security
    - [ ] Performed developer testing
    - [ ] Checklist finalized / ready for review

--- src/backend/api/Fusion.Resources.Domain/Models/QueryOrgUnitReason.cs
+++ src/backend/api/Fusion.Resources.Domain/Models/QueryOrgUnitReason.cs
@@ -1,9 +1,44 @@
-﻿namespace Fusion.Resources.Domain.Models
+﻿using System.Collections.Generic;
+using System;
+
+namespace Fusion.Resources.Domain.Models
 {
     internal class QueryOrgUnitReason
     {
+        public QueryOrgUnitReason(string fullDepartment, string reason)
+        {
+            IsWildCard = fullDepartment.Trim().EndsWith('*');
+            Reason = reason;
+            FullDepartment = fullDepartment.Replace("*", "").Trim();
+            Level = FullDepartment.Split(" ").Length;
+            IsGlobalRole = string.IsNullOrEmpty(fullDepartment);
+
+        }
+
         public string FullDepartment { get; set; } = null!;
         public string Reason { get; set; } = null!;
-        public bool IsWildCard => FullDepartment.Contains('*')  ;
+        public bool IsWildCard { get; set; }
+        public int Level { get; set; }
+        public bool IsGlobalRole { get; set; }
+    }
+
+    internal struct OrgUnitComparer
+    {
+        public OrgUnitComparer(string FullDepartment)
+        {
+            this.FullDepartment = FullDepartment;
+            Level = FullDepartment.Split(" ").Length;
+        }
+
+        public string FullDepartment { get; }
+        public int Level { get; set; }
+
+        public bool IsChildOf(OrgUnitComparer other, int maxDistance)
+        {
+            var isChild = other.FullDepartment.StartsWith(FullDepartment + " ", StringComparison.OrdinalIgnoreCase);
+            var distance = Level - other.Level;
+
+            return isChild && distance <= maxDistance;
+        }
     }
 }
