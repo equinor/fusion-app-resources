a4e25e5ad81ee0a57948361565278e51a2a99745
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2025-04-28 14:01:32 +0200

    Added tests

---
+++ src/tests/Fusion.Summary.Api.Tests/Helpers/DateUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Fusion.Summary.Api.Tests.Helpers;
+
+public static class DateUtils
+{
+    public static DateTime GetPreviousWeeksMonday(DateTime date)
+    {
+        switch (date.DayOfWeek)
+        {
+            case DayOfWeek.Sunday:
+                return date.AddDays(-6);
+            case DayOfWeek.Monday:
+                return date.AddDays(-7);
+            case DayOfWeek.Tuesday:
+            case DayOfWeek.Wednesday:
+            case DayOfWeek.Thursday:
+            case DayOfWeek.Friday:
+            case DayOfWeek.Saturday:
+            default:
+            {
+                // Calculate days until previous monday
+                // Go one week back and then remove the days until the monday
+                var daysUntilLastWeeksMonday = 1 - (int)date.DayOfWeek - 7;
+
+                return date.AddDays(daysUntilLastWeeksMonday);
+            }
+        }
+    }
+}
