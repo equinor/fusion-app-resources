cfc627255623d7395d78111cb86f22e80eca5b30
Jonathan Idland Olsnes <73334350+Jonathanio123@users.noreply.github.com>
2025-05-08 10:01:52 +0200

    Make DelegatedResourceOwnerForDepartment a const

--- src/backend/api/Fusion.Resources.Domain/Authorization/ResourcesClaimTypes.cs
+++ src/backend/api/Fusion.Resources.Domain/Authorization/ResourcesClaimTypes.cs
@@ -5,7 +5,7 @@ namespace Fusion.Resources
     public static class ResourcesClaimTypes
     {
         public const string ResourceOwnerForDepartment = "Fusion.Resources.ResourceOwnerForDepartment";
-        public static string DelegatedResourceOwnerForDepartment = "Fusion.Resources.DelegatedResourceOwnerForDepartment";
+        public const string DelegatedResourceOwnerForDepartment = "Fusion.Resources.DelegatedResourceOwnerForDepartment";
         public const string Prefix = "Fusion.Resources.Request.";
         public static string BasicRead = $"{Prefix}{SharedRequestScopes.BasicRead}";
     }
