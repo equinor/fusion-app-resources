72347bbaf7398585cea8dfad7dce3f8e2f071465
Anders Nikolas Drummond Mundal <anmun@equinor.com>
2025-05-30 10:53:00 +0200

    feat: add parent to related departments endpoint

--- src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/ApiRelatedDepartments.cs
+++ src/backend/api/Fusion.Resources.Api/Controllers/Departments/ApiModels/ApiRelatedDepartments.cs
@@ -8,10 +8,12 @@ namespace Fusion.Resources.Api.Controllers
     {
         public ApiRelatedDepartments(QueryRelatedDepartments relevantDepartments)
         {
+            Parent = new ApiDepartment(relevantDepartments.Parent);
             Children = relevantDepartments.Children.Select(x => new ApiDepartment(x)).ToList();
             Siblings = relevantDepartments.Siblings.Select(x => new ApiDepartment(x)).ToList();
         }
 
+        public ApiDepartment Parent { get; }
         public List<ApiDepartment> Children { get; }
         public List<ApiDepartment> Siblings { get; }
     }
--- src/backend/api/Fusion.Resources.Domain/Models/QueryRelatedDepartments.cs
+++ src/backend/api/Fusion.Resources.Domain/Models/QueryRelatedDepartments.cs
@@ -4,6 +4,7 @@ namespace Fusion.Resources.Domain
 {
     public class QueryRelatedDepartments
     {
+        public QueryDepartment Parent { get; set; } = null!;
         public List<QueryDepartment> Children { get; set; } = new List<QueryDepartment>();
         public List<QueryDepartment> Siblings { get; set; } = new List<QueryDepartment>();
     }
