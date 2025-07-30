08c00fe2f6f97cef3fa6a637e304180ec25d4a49
Mustafa-Hersi <mustafa.hersi@bouvet.no>
2025-07-30 14:05:46 +0200

    feat: db migration of summaryreports table

---
+++ src/Fusion.Summary.Api/Database/Migrations/20250730120409_OpenRequestsWorkload_to_db_column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fusion.Summary.Api.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class OpenRequestsWorkload_to_db_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OpenRequestsWorkload",
+                table: "WeeklySummaryReports",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OpenRequestsWorkload",
+                table: "WeeklySummaryReports");
+        }
+    }
+}
